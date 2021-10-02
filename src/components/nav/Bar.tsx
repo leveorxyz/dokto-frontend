d187a22cfa40245a312a315863201745dd6ea552
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-02 17:01:11 +0600

    implement navbar styles
    TODO: implement responsiveness

--- src/components/nav/Bar.tsx
+++ src/components/nav/Bar.tsx
@@ -1,15 +1,42 @@
 import React from "react";
+import { Flex, Box, Image, Button, ButtonGroup } from "@chakra-ui/react";
 
 import routes from "../../router/routes";
 import NavItem from "./NavItem";
+import logoSVG from "../../static/logo.svg";
+import { SpacedContainer } from "../common/Containers";
+
+function Logo() {
+	return(
+		<Box>
+			<Image src={logoSVG} alt="logo" boxSize="3rem" />
+		</Box>
+	);
+}
+
+function AuthButtons() {
+	return(
+		<ButtonGroup colorScheme="cyan" isAttached isRounded>
+			<Button mr="-px">Login</Button>
+			<Button mr="-px">Register</Button>
+		</ButtonGroup>
+	);
+}
 
 export default function Bar() {
 	return (
-		<>
-			{routes.map((route) => <NavItem 
-				key={route.path} 
-				title={route.displayName}
-				href={route.path}
-			/>)}
-		</>);
+		<SpacedContainer py={4}>
+			<Flex justifyContent="space-between" alignItems="center">
+				<Logo />
+				<Flex direction={["column", "column", "row", "row"]}>
+					{routes.map((route) => <NavItem 
+						key={route.path} 
+						title={route.displayName}
+						href={route.path}
+					/>)}
+				</Flex>
+				<AuthButtons />
+			</Flex>
+		</SpacedContainer>
+	);
 }
--- src/components/nav/NavItem.tsx
+++ src/components/nav/NavItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link as RouteLink } from "react-router-dom";
-import { Link } from "@chakra-ui/react";
+import { Link as RouteLink, useLocation } from "react-router-dom";
+import { Link, Box } from "@chakra-ui/react";
 
 type PropTypes = {
   href: string;
@@ -9,9 +9,36 @@ type PropTypes = {
 }
 
 export default function NavItem({href, icon, title}: PropTypes) {
+	const location = useLocation();
+
 	return (
-		<Link as={RouteLink} to={href}>
-			{icon}{icon && <>&nbsp;</>}{title}
-		</Link>
+		<Box p={3}>
+			<Link as={RouteLink} 
+				to={href}
+				fontSize="md"
+				fontWeight={600}
+				position="relative"
+				color="gray.700"
+				style={{
+					textDecoration: "none",
+				}}
+				_after={{
+					content: "''",
+					position: "absolute",
+					bottom: "-6px",
+					left: 0,
+					width: "100%",
+					height: location.pathname === href
+						? "3px"
+						: "0px",
+					backgroundColor: "pink.500",
+				}}
+				_hover={{
+					color: "pink.500",
+				}}
+			>
+				{icon}{icon && <>&nbsp;</>}{title}
+			</Link>
+		</Box>
 	);
 }
