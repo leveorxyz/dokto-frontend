e423472c9f4cdc1922e13f6fd5accf8a7e157cd1
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-02 21:13:11 +0600

    fix svg type

--- customs.d.ts
+++ customs.d.ts
@@ -1,4 +1,6 @@
 declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  const content: 
+    React.FunctionComponent<React.SVGAttributes<SVGElement>> |
+    string;
   export default content;
 }
