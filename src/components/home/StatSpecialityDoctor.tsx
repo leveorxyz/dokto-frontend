ac4efe4240888a85f456342d8f61725c0c97db12
Afia Anjum <afiatamanna06@gmail.com>
2021-10-04 00:44:57 +0600

    Update styles in StatSpeciality cards

--- src/components/home/StatSpecialityDoctor.tsx
+++ src/components/home/StatSpecialityDoctor.tsx
@@ -5,10 +5,10 @@ const StatSpecialityDoctor = () => {
 	return (
 		<div>
 			<Grid templateColumns='repeat(2, 1fr)' templateRows='repeat(2, 1fr)' gap={6} w='100%'>
-				<Box textAlign='center' border='1px' borderColor='black' p='100'>75</Box>
-				<Box textAlign='center' border='1px' borderColor='black' p='100'>45</Box>
-				<Box textAlign='center' border='1px' borderColor='black' p='100'>35</Box>
-				<Box textAlign='center' border='1px' borderColor='black' p='100'>5</Box>
+				<Box textAlign='center' border='1px' borderColor='black' p='100'><Box fontSize='xx-large' fontWeight='black'>75</Box><Box fontSize='sm' fontWeight='bold'>Professional Staff</Box></Box>
+				<Box textAlign='center' border='1px' borderColor='black' p='100'><Box fontSize='xx-large' fontWeight='black'>45</Box><Box fontSize='sm' fontWeight='bold'>Rooms</Box></Box>
+				<Box textAlign='center' border='1px' borderColor='black' p='100'><Box fontSize='xx-large' fontWeight='black'>35</Box><Box fontSize='sm' fontWeight='bold'>Years Experience</Box></Box>
+				<Box textAlign='center' border='1px' borderColor='black' p='100'><Box fontSize='xx-large' fontWeight='black'>5</Box><Box fontSize='sm' fontWeight='bold'>Stars Comfort</Box></Box>
 			</Grid>
 		</div>
 	);
