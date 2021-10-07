3a9ff82ca8d1aac44117acf0ce42e9b79594766c
Afia Anjum <afiatamanna06@gmail.com>
2021-10-07 13:26:51 +0600

    Add Footer in HowItWorks page

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import DescriptionWorkDoctor from "../components/how_it_works/DescriptionWorkDoctor";
 import HeaderWorkDoctor from "../components/how_it_works/HeaderWorkDoctor";
 import ProcessWorkDoctor from "../components/how_it_works/ProcessWorkDoctor";
+import FooterDoctor from "../components/home/FooterDoctor";
 
 const HowItWorks = () => (
   <div>
     <HeaderWorkDoctor />
     <DescriptionWorkDoctor />
     <ProcessWorkDoctor />
+    <FooterDoctor />
   </div>
 );
 
