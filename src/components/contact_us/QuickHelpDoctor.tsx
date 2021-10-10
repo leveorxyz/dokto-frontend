f933edb1850ca331206e785ca3a0f89d7397ca33
Afia Anjum <afiatamanna06@gmail.com>
2021-10-11 00:45:20 +0600

    Initialize QuickHelpDoctor file

---
+++ src/components/contact_us/QuickHelpDoctor.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+const QuickHelpDoctor = () => (
+  <div>
+    <Box>
+      <Heading>Quick Help</Heading>
+    </Box>
+  </div>
+);
+
+export default QuickHelpDoctor;
