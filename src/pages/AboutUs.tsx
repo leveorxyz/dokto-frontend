6c046c296a83b59883f9132064574766690685a6
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-11 04:11:52 +0600

    refactor common/CommonHeader to common/Banner and fix UI

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import FooterDoctor from "../components/home/FooterDoctor";
 import AboutUsDescription from "../components/about_us/AboutUsDescription";
-import CommonHeader from "../components/common/CommonHeader";
+import Banner from "../components/common/Banner";
 import AboutUsFacts from "../components/about_us/AboutUsFacts";
 import MemberEngagementDoctor from "../components/about_us/MemberEngagementDoctor";
 
 const AboutUs = () => (
   <div>
-    <CommonHeader text="About Us" />
+    <Banner text="About Us" imgSrc="https://source.unsplash.com/collection/42413461/500x300" />
     <AboutUsFacts />
     <MemberEngagementDoctor />
     <AboutUsDescription />
