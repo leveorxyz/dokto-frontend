7b86af1cd700e83b3a7f835c5265e5ceb0dbd6db
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-11 07:28:54 +0600

    refactor

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,15 +1,12 @@
-import { Box } from "@chakra-ui/react";
 import React from "react";
-import QuickHelpDoctor from "../components/contact_us/QuickHelpDoctor";
+import QuickHelp from "../components/contact_us/QuickHelp";
 import Banner from "../components/common/Banner";
 
 const ContactUs = () => (
-  <div>
-    <Box>
-      <Banner text="Contact Us" imgSrc="https://source.unsplash.com/collection/42413461/500x300" />
-      <QuickHelpDoctor />
-    </Box>
-  </div>
+  <>
+    <Banner text="Contact Us" imgSrc="https://source.unsplash.com/collection/42413461/500x300" />
+    <QuickHelp />
+  </>
 );
 
 export default ContactUs;
