366b840936084ec03c24b4137ec7accca1b551ab
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-11 07:29:11 +0600

    add footer

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import QuickHelp from "../components/contact_us/QuickHelp";
 import Banner from "../components/common/Banner";
+import FooterDoctor from "../components/home/FooterDoctor";
 
 const ContactUs = () => (
   <>
     <Banner text="Contact Us" imgSrc="https://source.unsplash.com/collection/42413461/500x300" />
     <QuickHelp />
+    <FooterDoctor />
   </>
 );
 
