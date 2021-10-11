cac1e35e904e9e86cc16a12b2809c490da493c9e
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-11 06:18:03 +0600

    fix color

--- src/components/how_it_works/Process.tsx
+++ src/components/how_it_works/Process.tsx
@@ -7,7 +7,7 @@ import Timeline from "./Timeline";
 const ProcessWorkDoctor = () => (
   <SpacedContainer py={24}>
     <Heading as="h2" fontWeight="bold" fontSize="3xl" mb="4">Our Process</Heading>
-    <Heading as="h4" fontSize="2xl" mb="4" color="#00ffff" fontWeight="normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Heading>
+    <Heading as="h4" fontSize="2xl" mb="4" color="cyan.500" fontWeight="normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Heading>
     <Box py={[12, 24, 24, 24, 24]}>
       <Timeline />
     </Box>
