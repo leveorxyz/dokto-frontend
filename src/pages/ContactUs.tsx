7653f92666f5e0789d7bbe85ef95c4a53746e74e
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-11 08:38:29 +0600

    implement MessageSupport section

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+import MessageSupport from "../components/contact_us/MessageSupport";
 import QuickHelp from "../components/contact_us/QuickHelp";
 import Banner from "../components/common/Banner";
 import FooterDoctor from "../components/home/FooterDoctor";
@@ -7,6 +9,7 @@ const ContactUs = () => (
   <>
     <Banner text="Contact Us" imgSrc="https://source.unsplash.com/collection/42413461/500x300" />
     <QuickHelp />
+    <MessageSupport />
     <FooterDoctor />
   </>
 );
