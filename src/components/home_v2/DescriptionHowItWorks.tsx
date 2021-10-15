e910b2b0913743a1eb666b64403003da2187a3c7
Afia Anjum <afiatamanna06@gmail.com>
2021-10-15 11:37:54 +0600

    Update how it works file style

--- src/components/home_v2/DescriptionHowItWorks.tsx
+++ src/components/home_v2/DescriptionHowItWorks.tsx
@@ -1,17 +1,16 @@
 /* eslint-disable max-len */
 import React from "react";
 import {
-  Box, Heading, Divider,
+  Box, Heading, Divider, Link,
 } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
 
 const DescriptionHowItWorks = () => (
   <div>
-    <Box>
+    <Box p="10">
       <Heading as="h2" fontWeight="bold" fontSize="2xl" textTransform="uppercase">How it works</Heading>
-      <Divider w="10" h="2" mb="20" />
-      <Box>We are a website that provides services in the health sector, you can consult  about your health, find a doctor according to your diseases, or find the nearest hospital. You also get the latest articles about the world of health and interesting health tips</Box>
-      <Link to="/">Read more</Link>
+      <Divider w="14" h="2" mb="6" />
+      <Box fontSize="md" color="gray" mb="2">We are a website that provides services in the health sector, you can consult  about your health, find a doctor according to your diseases, or find the nearest hospital. You also get the latest articles about the world of health and interesting health tips.</Box>
+      <Link href="/" color="cyan.500" _hover={{ outline: "none" }}>Read more</Link>
     </Box>
   </div>
 );
--- src/components/home_v2/HowItWorks.tsx
+++ src/components/home_v2/HowItWorks.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import {
-  Box, Grid,
+  Box, Flex, Image,
 } from "@chakra-ui/react";
 import { SpacedContainer } from "../common/Containers";
 import DescriptionHowItWorks from "./DescriptionHowItWorks";
 
 const HowItWorks = () => (
   <div>
-    <SpacedContainer>
-      <Grid>
-        <Box />
-        <DescriptionHowItWorks />
-      </Grid>
+    <SpacedContainer mb="100">
+      <Flex
+        direction={{
+          sm: "column", md: "row", lg: "row", xl: "row", base: "column",
+        }}
+        alignItems="center"
+      >
+        <Box flex="1"><Image src="https://source.unsplash.com/collection/42413461/500x300" fit="contain" overflow="hidden" /></Box>
+        <Box flex="1"><DescriptionHowItWorks /></Box>
+      </Flex>
     </SpacedContainer>
   </div>
 );
