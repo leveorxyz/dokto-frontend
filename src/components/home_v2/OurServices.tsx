e80494e10dd5b8ad835d8e25d0c427d432c1ca13
Afia Anjum <afiatamanna06@gmail.com>
2021-10-15 09:58:40 +0600

    Add our services data

--- src/components/home_v2/OurServices.tsx
+++ src/components/home_v2/OurServices.tsx
@@ -2,83 +2,10 @@ import React from "react";
 import {
   Box, Heading, Grid, Center, Divider,
 } from "@chakra-ui/react";
-import { IoChatbubbleOutline } from "react-icons/io5";
-import { FaRegHospital, FaCapsules, FaStethoscope } from "react-icons/fa";
+
 import OurServicesCard from "./OurServicesCard";
 import { SpacedContainer } from "../common/Containers";
-
-const facts = [
-  {
-    id: "people",
-    Icon: IoChatbubbleOutline,
-    title: (
-      <>
-        <b>Consulation</b>
-      </>
-    ),
-    description: (
-      <>
-        Consulation with a
-        <br />
-        trusted doctor
-      </>
-    ),
-  },
-  {
-    id: "globe",
-    Icon: FaStethoscope,
-    title: (
-      <>
-        <b>Doctor</b>
-      </>
-    ),
-    description: (
-      <>
-        Thousands of doctors
-        <br />
-        with specific or general
-        <br />
-        expertise
-      </>
-    ),
-  },
-  {
-    id: "business",
-    Icon: FaRegHospital,
-    title: (
-      <>
-        <b>Hospital</b>
-      </>
-    ),
-    description: (
-      <>
-        Look for the hospital
-        <br />
-        closest to your
-        <br />
-        location
-      </>
-    ),
-  },
-  {
-    id: "pharmacy",
-    Icon: FaCapsules,
-    title: (
-      <>
-        <b>Online pharmacy</b>
-      </>
-    ),
-    description: (
-      <>
-        Find a drug that
-        <br />
-        matches your diseases,
-        <br />
-        or buy it directly
-      </>
-    ),
-  },
-];
+import OurServicesData from "./OurServicesData";
 
 export default function OurServices() {
   return (
@@ -94,7 +21,7 @@ export default function OurServices() {
               }}
               gap={6}
             >
-              {facts.map(({
+              {OurServicesData.map(({
                 id, Icon, title, description,
               }) => (
                 <OurServicesCard
