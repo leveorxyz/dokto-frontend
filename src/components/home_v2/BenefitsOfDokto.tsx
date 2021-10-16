3a2aae865a02b9924a971d20ee1d1c0c43aa50ef
Afia Anjum <afiatamanna06@gmail.com>
2021-10-16 14:11:52 +0600

    Update Buttongroups styles in benefitsofdokto

--- src/components/home_v2/BenefitsOfDokto.tsx
+++ src/components/home_v2/BenefitsOfDokto.tsx
@@ -6,11 +6,11 @@ const BenefitsOfDokto = () => (
   <div>
     <SpacedContainer py="50">
       <Heading as="h2" fontSize="3xl" fontWeight="bold">Benefits of Dokto</Heading>
-      <ButtonGroup d="flex" flexDir="row" mt="50" isAttached>
-        <Button flex="1">Patient</Button>
-        <Button flex="1">Doctor</Button>
-        <Button flex="1">Hospital</Button>
-        <Button flex="1">Pharmacy</Button>
+      <ButtonGroup d="flex" flexDir="row" mt="50" border="1px" borderRadius="lg" borderColor="blueviolet" isAttached>
+        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Patient</Button>
+        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Doctor</Button>
+        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Hospital</Button>
+        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Pharmacy</Button>
       </ButtonGroup>
     </SpacedContainer>
   </div>
