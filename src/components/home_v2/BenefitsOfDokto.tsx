d8e7b786b363fe635281545d8aaaf5f5635ee607
Afia Anjum <afiatamanna06@gmail.com>
2021-10-16 14:21:42 +0600

    Add buttonlists in benefitsofdokto

--- src/components/home_v2/BenefitsOfDokto.tsx
+++ src/components/home_v2/BenefitsOfDokto.tsx
@@ -2,18 +2,25 @@ import React from "react";
 import { Button, ButtonGroup, Heading } from "@chakra-ui/react";
 import { SpacedContainer } from "../common/Containers";
 
-const BenefitsOfDokto = () => (
-  <div>
-    <SpacedContainer py="50">
-      <Heading as="h2" fontSize="3xl" fontWeight="bold">Benefits of Dokto</Heading>
-      <ButtonGroup d="flex" flexDir="row" mt="50" border="1px" borderRadius="lg" borderColor="blueviolet" isAttached>
-        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Patient</Button>
-        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Doctor</Button>
-        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Hospital</Button>
-        <Button flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">Pharmacy</Button>
-      </ButtonGroup>
-    </SpacedContainer>
-  </div>
-);
+const BenefitsOfDokto = () => {
+  const buttons = [
+    { content: "Patient" },
+    { content: "Doctor" },
+    { content: "Hospital" },
+    { content: "Pharmacy" },
+  ];
 
+  return (
+    <div>
+      <SpacedContainer py="50">
+        <Heading as="h2" fontSize="3xl" fontWeight="bold">Benefits of Dokto</Heading>
+        <ButtonGroup d="flex" flexDir="row" mt="50" border="1px" borderRadius="lg" borderColor="blueviolet" isAttached>
+          {buttons?.map(({ content }) => (
+            <Button key={content} flex="1" py="7" textTransform="uppercase" bgColor="white" color="cyan.700">{content}</Button>
+          ))}
+        </ButtonGroup>
+      </SpacedContainer>
+    </div>
+  );
+};
 export default BenefitsOfDokto;
