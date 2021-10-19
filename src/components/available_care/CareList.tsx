316673e319d1ea7089747d4d9278245ca66cd9a2
Afia Anjum <afiatamanna06@gmail.com>
2021-10-19 16:47:34 +0600

    Update Care list bottom description

--- src/components/available_care/CareList.tsx
+++ src/components/available_care/CareList.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 import React from "react";
-import { Grid } from "@chakra-ui/react";
+import { Grid, Box } from "@chakra-ui/react";
 import { SpacedContainer } from "../common/Containers";
 import CareListData from "./CareListData";
 import CareListCard from "./CareListCard";
@@ -23,11 +24,17 @@ const CareList = () => (
           sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)", lg: "repeat(3, 1fr)", base: "repeat(1, 1fr)",
         }}
         gap={20}
+        mb="20"
       >
         <CareListCard listData={CareListData.slice(18, 25)} />
         <CareListCard listData={CareListData.slice(25, 31)} />
         <CareListCard listData={CareListData.slice(31, CareListData.length)} />
       </Grid>
+      <Box fontSize="md" color="gray" textAlign="center" px="2" mb="20">
+        *Some services may require additional or in-person follow-up consultation with a specialized practitioner, which may not be available online.
+        <br />
+        Please sign in to your account access the available services.
+      </Box>
     </SpacedContainer>
   </div>
 );
