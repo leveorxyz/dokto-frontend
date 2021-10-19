026aac1d7fa7f29600acf979e9f94ade3befd05f
Afia Anjum <afiatamanna06@gmail.com>
2021-10-19 19:28:51 +0600

    Add spacing in available care

--- src/components/available_care/AvailableCareHeader.tsx
+++ src/components/available_care/AvailableCareHeader.tsx
@@ -4,7 +4,7 @@ import { SpacedContainer } from "../common/Containers";
 
 const AvailableCareHeader = () => (
   <div>
-    <SpacedContainer>
+    <SpacedContainer py="20">
       <Heading as="h1" fontWeight="bold" fontSize="8xl">Available Care</Heading>
       <Heading as="h2" fontWeight="bold" fontSize="2xl" color="purple" mb="4">What We Treat</Heading>
       <Box fontSize="md" color="gray" mb="2">
