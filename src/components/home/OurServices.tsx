b2d590d8d7b81fc42fedf851dae845797a921fb4
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-24 00:09:10 +0600

    fix OurServices styles

--- src/components/home/OurServices.tsx
+++ src/components/home/OurServices.tsx
@@ -9,41 +9,38 @@ import OurServicesData from "./OurServicesData";
 
 export default function OurServices() {
   return (
-    <div>
-      <Box w="100%" py="100">
-        <SpacedContainer>
-          <Center display="flex" flexDir="column" borderColor="plum">
-            <Heading as="h2" fontWeight="bold" fontSize="2xl" textTransform="uppercase">Our Services</Heading>
-            <Divider w="10" h="2" mb="100" />
-            <Grid
-              templateColumns={{
-                sm: "repeat(1, 1fr)", md: "repeat(4, 1fr)", lg: "repeat(4, 1fr)", base: "repeat(1, 1fr)",
-              }}
-              gap={6}
-            >
-              {OurServicesData.map(({
-                id, Icon, title, description,
-              }) => (
-                <OurServicesCard
-                  key={id}
-                  icon={(
-                    <Box
-                      padding={4}
-                      bg="cyan.500"
-                      rounded="full"
-                      w="min-content"
-                    >
-                      <Icon size={28} />
-                    </Box>
-        )}
-                  title={title}
-                  description={description}
-                />
-              ))}
-            </Grid>
-          </Center>
-        </SpacedContainer>
-      </Box>
-    </div>
+    <SpacedContainer py={24}>
+      <Center display="flex" flexDir="column" borderColor="plum">
+        <Heading as="h2" fontWeight="bold" fontSize="2xl" textTransform="uppercase">Our Services</Heading>
+        <Divider w="10" h="2" mb="100" />
+        <Grid
+          width="100%"
+          templateColumns={{
+            base: "repeat(1, 1fr)", sm: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(4, 1fr)",
+          }}
+          gap={[6, 6, 12, 24, 24]}
+        >
+          {OurServicesData.map(({
+            id, Icon, title, description,
+          }) => (
+            <OurServicesCard
+              key={id}
+              icon={(
+                <Box
+                  padding={4}
+                  bg="cyan.500"
+                  rounded="full"
+                  w="min-content"
+                >
+                  <Icon size={28} />
+                </Box>
+              )}
+              title={title}
+              description={description}
+            />
+          ))}
+        </Grid>
+      </Center>
+    </SpacedContainer>
   );
 }
--- src/components/home/OurServicesCard.tsx
+++ src/components/home/OurServicesCard.tsx
@@ -8,19 +8,24 @@ type PropTypes = {
 }
 
 const OurServicesCard = ({ icon, title, description }: PropTypes) => (
-  <div>
-    <Box w="100%" height="100%" borderRadius="20" _hover={{ bg: "white", boxShadow: "lg" }} p="6" cursor="pointer">
-      <Flex direction="column" px="1">
-        {icon}
-        <Text fontSize="xl" mt="4" mb="2">
-          {title}
-        </Text>
-        <Text fontSize="md" color="gray">
-          {description}
-        </Text>
-      </Flex>
-    </Box>
-  </div>
+  <Box
+    w="100%"
+    p={6}
+    height="100%"
+    borderRadius="20"
+    _hover={{ bg: "white", boxShadow: "0 10px 30px 0 rgb(205 204 219 / 25%)" }}
+    cursor="pointer"
+  >
+    <Flex direction="column" px="1">
+      {icon}
+      <Text fontSize="xl" mt="4" mb="2">
+        {title}
+      </Text>
+      <Text fontSize="md" color="gray">
+        {description}
+      </Text>
+    </Flex>
+  </Box>
 );
 
 export default OurServicesCard;
