7c4559099d3533aa8f6c4ff9eb64e87f1d1f1afa
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-24 00:51:22 +0600

    fix OurDoctors styles

--- src/components/home/OurDoctors.tsx
+++ src/components/home/OurDoctors.tsx
@@ -8,26 +8,24 @@ import { SpacedContainer } from "../common/Containers";
 import OurFeaturedDoctors from "./OurFeaturedDoctors";
 
 const OurDoctors = () => (
-  <div>
-    <Box w="100%" bg="gray.100">
-      <SpacedContainer py="16">
-        <Box d="flex" flexDir="column" alignItems="center">
-          <Heading as="h2" fontWeight="bold" fontSize="2xl" textTransform="uppercase">Our doctors</Heading>
-          <Divider w="14" h="2" mb="12" />
-          <Container maxW="container.xl">
-            <Box d="flex" justifyContent="space-between" alignItems="center" mb="7">
-              <Box><Heading as="h2" fontSize="3xl" alignItems="center" fontWeight="bold">Featured Doctors</Heading></Box>
-              <Box>
-                <LeftRightArrow />
-              </Box>
+  <Box bg="gray.100">
+    <SpacedContainer py="16">
+      <Box d="flex" flexDir="column" alignItems="center">
+        <Heading as="h2" fontWeight="bold" fontSize="2xl" textTransform="uppercase">Our doctors</Heading>
+        <Divider w="14" h="2" mb="12" />
+        <Container maxW="100%">
+          <Box d="flex" justifyContent="space-between" alignItems="center" mb="7">
+            <Box><Heading as="h2" fontSize="3xl" alignItems="center" fontWeight="bold">Featured Doctors</Heading></Box>
+            <Box>
+              <LeftRightArrow />
             </Box>
-            <OurFeaturedDoctors />
-          </Container>
-          <Link href="/" mt="14" fontSize="lg" cursor="pointer" fontWeight="bold" _hover={{ outline: "none" }}>View All Doctors</Link>
-        </Box>
-      </SpacedContainer>
-    </Box>
-  </div>
+          </Box>
+          <OurFeaturedDoctors />
+        </Container>
+        <Link href="/" mt="14" fontSize="lg" cursor="pointer" fontWeight="bold" _hover={{ outline: "none" }}>View All Doctors</Link>
+      </Box>
+    </SpacedContainer>
+  </Box>
 );
 
 export default OurDoctors;
