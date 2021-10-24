a31d44b6b83d64eeeb125475c942635accbf1214
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-25 05:54:45 +0600

    Fix chart data points

--- src/components/home/HospitalSection.tsx
+++ src/components/home/HospitalSection.tsx
@@ -15,7 +15,7 @@ const HospitalSection = () => {
   ];
   const options = {
     xaxis: {
-      categories: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+      categories: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
     },
   };
   return (
