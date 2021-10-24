11401d13833277509cdb39e7abfdd502354ad5b4
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-25 04:26:58 +0600

    Add fonts to theme

--- src/components/ExtendedTheme.tsx
+++ src/components/ExtendedTheme.tsx
@@ -2,8 +2,8 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
-    heading: "Inter",
-    body: "Inter",
+    heading: "Poppins",
+    body: "Sora",
   },
   colors: {
     brand: {
