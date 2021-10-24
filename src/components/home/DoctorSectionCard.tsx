8a199b2b4b34310e5bde0fb12d0c22d6004658c4
Afia Anjum <afiatamanna06@gmail.com>
2021-10-24 22:50:04 +0600

    Update benefits of dokto patient and doctor section style

--- src/components/home/DoctorSectionCard.tsx
+++ src/components/home/DoctorSectionCard.tsx
@@ -7,9 +7,9 @@ type PropTypes = {
 }
 
 const DoctorSectionCard = ({ image, text }:PropTypes) => (
-  <Box w="100%" d="flex" flexDir={["column", "column", "row", "row"]} alignItems="center" p="3">
+  <Box w="100%" d="flex" flexDir={["column", "column", "row", "row"]} alignItems="top" m="4">
     <Image src={image} w={["100%", "100%", "25%", "25%"]} objectFit="cover" />
-    <Box fontSize="md" py={4} px={[0, 0, 4, 4, 4]} fontWeight="semibold" color="#170041">{text}</Box>
+    <Box fontSize="md" px={[0, 0, 6, 6, 6]} fontWeight="semibold" color="#170041">{text}</Box>
   </Box>
 );
 
