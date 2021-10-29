5b5bcf37aa4ff440580f71242f88cf107b92a7c6
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-10-29 06:20:18 +0600

    Update InputField to use addons and elements

--- src/components/forms/common/InputField.tsx
+++ src/components/forms/common/InputField.tsx
@@ -1,5 +1,10 @@
 import {
   Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  InputLeftElement,
+  InputRightElement,
   FormControl,
   FormLabel,
   FormErrorMessage,
@@ -13,17 +18,33 @@ type DefaultProps = {
 } & Omit<InputFieldType, "type">;
 
 export default function InputField({
-  name, placeholder, inputType, label, errors, register, rules,
+  name,
+  placeholder,
+  inputType,
+  leftAddon,
+  rightAddon,
+  rightElement,
+  leftElement,
+  label,
+  errors,
+  register,
+  rules,
 }: DefaultProps) {
   return (
     <FormControl isInvalid={errors[name]}>
       <FormLabel htmlFor="name">{label}</FormLabel>
-      <Input
-        id={name}
-        type={inputType}
-        placeholder={placeholder}
-        {...register(name, rules)}
-      />
+      <InputGroup>
+        {leftAddon && <InputLeftAddon>{leftAddon}</InputLeftAddon>}
+        {leftElement && <InputLeftElement>{leftElement}</InputLeftElement>}
+        <Input
+          id={name}
+          type={inputType}
+          placeholder={placeholder}
+          {...register(name, rules)}
+        />
+        {rightAddon && <InputRightAddon>{rightAddon}</InputRightAddon>}
+        {rightElement && <InputRightElement>{rightElement}</InputRightElement>}
+      </InputGroup>
       <FormErrorMessage>
         {errors[name] && errors[name].message}
       </FormErrorMessage>
