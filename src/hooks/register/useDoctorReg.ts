3d14de69d7a1651e2a19c06df80f42ea6e5f4104
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-02 05:04:07 +0600

    Implement useDoctorReg hook

---
+++ src/hooks/register/useDoctorReg.ts
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { useQuery } from "react-query";
+import { AxiosInstance } from "axios";
+import { useSetRecoilState } from "recoil";
+
+import { AxiosContext } from "../../contexts/AxiosContext";
+import authAtom from "../../atoms/auth.atom";
+
+const registerDoctor = async (axios: AxiosInstance, data: any) => {
+  const response = await axios.post("user/doctor-signup", data);
+  return response;
+};
+
+export default function useDoctorReg(data: any) {
+  const axios = useContext<AxiosInstance | null>(AxiosContext);
+  const setAuthState = useSetRecoilState(authAtom);
+
+  console.log({ data, axios, setAuthState });
+
+  return useQuery(
+    ["doctor_reg", data],
+    () => registerDoctor(axios as AxiosInstance, data),
+    {
+      onSuccess: ({ data: response }) => {
+        setAuthState({
+          isLoggedIn: true,
+          user: {
+            id: response.id,
+            email: response.email,
+            token: response.token,
+            profilePhoto: response.profile_photo ?? "",
+          },
+        });
+      },
+      onError: (err) => Promise.reject(err),
+    },
+  );
+}
