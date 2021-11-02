3f7915c973ae8391d12c4c67da500a92f00ca842
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-03 01:35:58 +0600

    Fix patient reg data formatting

--- src/components/forms/patient-registration/steps/Submit.tsx
+++ src/components/forms/patient-registration/steps/Submit.tsx
@@ -5,20 +5,29 @@ import { Country, State } from "country-state-city";
 import { stepAtom } from "../atoms";
 
 export default function Submit() {
-  const stepData = useRecoilValue(stepAtom);
+  const stepData = useRecoilValue<any>(stepAtom);
   const data = useMemo(() => ({
     ...Object.keys(stepData).reduce(
       (prev, curr) => {
         if (curr === "country") {
-          return { ...prev, [curr]: Country.getCountryByCode((stepData as any)[curr])?.name };
+          return { ...prev, [curr]: Country.getCountryByCode(stepData[curr])?.name };
         }
         if (curr === "state") {
-          if ((stepData as any)[curr] === "") return prev;
-          return { ...prev, [curr]: State.getStateByCode((stepData as any)[curr])?.name };
+          if (stepData[curr] === "") return prev;
+          return { ...prev, [curr]: State.getStateByCode(stepData[curr])?.name };
         }
-        return { ...prev, [curr]: (stepData as any)[curr] };
+        if (curr === "first_name") {
+          return { ...prev, full_name: `${stepData[curr]} ${prev.full_name.trim()}` };
+        }
+        if (curr === "last_name") {
+          return { ...prev, full_name: `${prev.full_name.trim()} ${stepData[curr]}` };
+        }
+        if (!stepData[curr] || stepData[curr] === "") return prev;
+        return { ...prev, [curr]: stepData[curr] };
       },
-      {},
+      {
+        full_name: "",
+      },
     ),
   }), [stepData]);
 
