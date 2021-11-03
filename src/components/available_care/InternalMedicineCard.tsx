5cc039d34bad7c326688911f30e409257d6ef616
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-04 04:39:59 +0600

    Update AvailableCare carousel component

--- src/components/available_care/InternalMedicineCard.tsx
+++ src/components/available_care/InternalMedicineCard.tsx
@@ -6,29 +6,31 @@ type PropTypes = {
 };
 
 const InternalMedicineCard = ({ title, data }: PropTypes) => (
-  <div>
+
+  <Box
+    bg="white"
+    d="flex"
+    p="10"
+    minH="28rem"
+    flexDir="column"
+    maxW="100%"
+    borderRadius="2xl"
+  >
     <Box
-      bg="white"
       d="flex"
-      p="10"
-      height="100%"
-      flexDir="column"
-      maxW="100%"
-      borderRadius="2xl"
+      flexDir="row"
+      alignItems="center"
+      mb="2"
     >
-      <Box
-        d="flex"
-        flexDir="row"
-        alignItems="center"
-        mb="2"
-      >
-        <Box borderWidth={2} p={1} borderColor="#A42BAD" rounded="full">
-          <Avatar name={title} bg="gray.200" color="gray.700" />
-        </Box>
+      <Box borderWidth={2} p={1} borderColor="#A42BAD" rounded="full">
+        <Avatar name={title} bg="gray.200" color="primary.dark" />
       </Box>
-      <Box color="rgba(51, 51, 51, 0.6)">{data}</Box>
+      <Box as="h3" ml="2" fontSize="lg" fontWeight="semibold" color="primary.dark">
+        {title}
+      </Box>
     </Box>
-  </div>
+    <Box color="rgba(51, 51, 51, 0.6)" mt={4}>{data}</Box>
+  </Box>
 );
 
 export default InternalMedicineCard;
