a0a2a0f9942af3c31e26eb15f1a3e27b9b7cdaca
Afia Anjum <afiatamanna06@gmail.com>
2021-11-05 12:16:15 +0600

    Update Professional Bio Array

--- src/components/forms/provider-registration/doctor/steps/step4_data.ts
+++ src/components/forms/provider-registration/doctor/steps/step4_data.ts
@@ -7,16 +7,86 @@ const firstColumn = () :FormDataType => ({
   name: "first",
   fields: [
     {
-      type: FieldTypes.TEXTAREA,
+      type: FieldTypes.ARRAY,
       name: "professional_bio",
-      label: "Add Professional Bio",
-      resize: "both",
+      label: "Professional Bio",
+      addButtonText: "Add Professional Bio +",
       rules: {
         required: {
           value: true,
           message: "This field is required",
         },
       },
+      fields: [
+        {
+          type: FieldTypes.INPUT,
+          name: "doctor_info",
+          label: "Doctor Info",
+          placeholder: "Doctor Info",
+        },
+        {
+          type: FieldTypes.INPUT,
+          name: "establishment_name",
+          label: "Establishment Name",
+          placeholder: "Establishment Name",
+          rules: {
+            required: {
+              value: true,
+              message: "This field is required",
+            },
+            maxLength: {
+              value: 50,
+              message: "Must be less than 50 characters",
+            },
+          },
+        },
+        {
+          type: FieldTypes.INPUT,
+          name: "job_title",
+          label: "Job Title",
+          placeholder: "Job Title",
+          rules: {
+            required: {
+              value: true,
+              message: "This field is required",
+            },
+            maxLength: {
+              value: 50,
+              message: "Must be less than 50 characters",
+            },
+          },
+        },
+        {
+          type: FieldTypes.INPUT,
+          name: "start_date",
+          label: "Start Date",
+          inputType: "date",
+          rules: {
+            required: {
+              value: true,
+              message: "This field is required",
+            },
+          },
+        },
+        {
+          type: FieldTypes.INPUT,
+          name: "end_date",
+          label: "End Date",
+          inputType: "date",
+        },
+        {
+          type: FieldTypes.TEXTAREA,
+          name: "job_description",
+          label: "Job Description",
+          resize: "both",
+          rules: {
+            maxLength: {
+              value: 200,
+              message: "Must be less than 200 characters",
+            },
+          },
+        },
+      ],
     },
     {
       type: FieldTypes.FILE,
