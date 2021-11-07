effc2e40f1b1551b658748740255ae09a3f3eca9
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-07 21:28:04 +0600

    Implement Participant component

---
+++ src/components/call/Participant.tsx
@@ -0,0 +1,98 @@
+import { useState, useEffect, useRef } from "react";
+import { AudioTrack, VideoTrack, Participant as VideoParticipant } from "twilio-video";
+
+interface ParticipantProps {
+  participant: VideoParticipant;
+}
+
+const Participant = ({ participant }: ParticipantProps) => {
+  const [videoTracks, setVideoTracks] = useState<(VideoTrack | null)[]>([]);
+  const [audioTracks, setAudioTracks] = useState<(AudioTrack | null)[]>([]);
+
+  // Create refs for the HTML elements to attach audio and video to in the DOM
+  // For now, set them to null
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const audioRef = useRef<HTMLMediaElement>(null);
+
+  // Get the audio and video tracks from the participant, filtering out the tracks that are null
+  const getExistingVideoTracks = (currentParticipant: VideoParticipant) => {
+    const videoPublications = Array.from(currentParticipant.videoTracks.values());
+    const existingVideoTracks = videoPublications
+      .map((publication) => publication.track)
+      .filter((track) => track !== null);
+    return existingVideoTracks;
+  };
+
+  const getExistingAudioTracks = (currentParticipant: VideoParticipant) => {
+    const audioPublications = Array.from(currentParticipant.audioTracks.values());
+    const existingAudioTracks = audioPublications
+      .map((publication) => publication.track)
+      .filter((track) => track !== null);
+    return existingAudioTracks;
+  };
+
+  // When a new track is added or removed, update the video and audio tracks in the state
+  useEffect(() => {
+    const trackSubscribed = (track: AudioTrack | VideoTrack) => {
+      if (track.kind === "video") {
+        setVideoTracks((prev) => [...prev, track]);
+      } else {
+        setAudioTracks((prev) => [...prev, track]);
+      }
+    };
+
+    const trackUnsubscribed = (track: AudioTrack | VideoTrack) => {
+      if (track.kind === "video") {
+        setVideoTracks((prev) => prev.filter((v) => v !== track));
+      } else {
+        setAudioTracks((prev) => prev.filter((a) => a !== track));
+      }
+    };
+
+    setVideoTracks(getExistingVideoTracks(participant));
+    setAudioTracks(getExistingAudioTracks(participant));
+
+    // Set up event listeners
+    participant.on("trackSubscribed", trackSubscribed);
+    participant.on("trackUnsubscribed", trackUnsubscribed);
+
+    // Clean up at the end by removing all the tracks and the event listeners
+    return () => {
+      setVideoTracks([]);
+      setAudioTracks([]);
+      participant.removeAllListeners();
+      participant.videoTracks
+        // eslint-disable-next-line no-param-reassign
+        .forEach((track) => { track.isEnabled = false; });
+    };
+  }, [participant]);
+
+  // When a new videoTrack or audioTrack is subscribed, add it to the DOM.
+  // When unsubscribed, detach it
+  useEffect(() => {
+    const videoTrack = videoTracks[0];
+    videoTrack?.attach(videoRef?.current ?? undefined);
+    return () => {
+      videoTrack?.detach();
+    };
+  }, [videoTracks]);
+
+  useEffect(() => {
+    const audioTrack = audioTracks[0];
+    audioTrack?.attach(audioRef?.current ?? undefined);
+    return () => {
+      audioTrack?.detach();
+    };
+  }, [audioTracks]);
+
+  return (
+    <div className="participant" id={participant.identity}>
+      {/* these videos wouldn't have captions so it's safe to disable */}
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <video ref={videoRef} autoPlay />
+      <audio ref={audioRef} autoPlay muted />
+    </div>
+  );
+};
+
+export default Participant;
