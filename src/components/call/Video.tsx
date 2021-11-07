7c94fa51e65eea701ffb5cf97872aa94b26c3cf7
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-07 21:28:04 +0600

    Implement Participant component

---
+++ src/components/call/Video.tsx
@@ -0,0 +1,70 @@
+import { Box, Flex, IconButton } from "@chakra-ui/react";
+import {
+  IoPause, IoVideocam, IoVolumeHigh,
+} from "react-icons/io5";
+import { MdCallEnd } from "react-icons/md";
+
+type PropTypes = {
+  videoRef: React.RefObject<HTMLVideoElement>
+  showOptions?: boolean
+}
+
+export default function Video({ videoRef, showOptions }: PropTypes) {
+  const buttons = [
+    {
+      icon: IoPause,
+      onClick: () => {},
+      title: "Pause Video",
+    },
+    {
+      icon: IoVideocam,
+      onClick: () => {},
+      title: "Start Video",
+    },
+    {
+      icon: IoVolumeHigh,
+      onClick: () => {},
+      title: "Mute Video",
+    },
+    {
+      icon: MdCallEnd,
+      onClick: () => {},
+      title: "Stop Video",
+    },
+  ];
+
+  return (
+    <Box w="100%" h="100%" position="relative">
+      {/* these videos wouldn't have captions so it's safe to disable the caption tracks */}
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <video ref={videoRef} autoPlay />
+
+      {showOptions && (
+        <Flex
+          position="absolute"
+          bottom="1rem"
+          left="50%"
+          transform="translateX(-50%)"
+        >
+          {
+            buttons.map(
+              ({ title, icon: Icon, onClick }) => (
+                <IconButton
+                  key={title}
+                  icon={<Icon />}
+                  onClick={onClick}
+                  aria-label={title}
+                  mx={2}
+                />
+              ),
+            )
+          }
+        </Flex>
+      )}
+    </Box>
+  );
+}
+
+Video.defaultProps = {
+  showOptions: true,
+};
