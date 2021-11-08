9566f7d1080bfda1ee800ccbb0cb6503c52d0dca
Mahmudul Alam <expelmahmud@gmail.com>
2021-11-08 17:00:48 +0600

    add: unique participants

--- src/components/call/Videos.tsx
+++ src/components/call/Videos.tsx
@@ -1,20 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import _ from "lodash";
 import Participant from "./Participant";
+import { participantsAtom } from "./atoms";
 
 type PropTypes = {
   room: any;
 };
 
 const Videos = ({ room }: PropTypes) => {
-  const [remoteParticipants, setRemoteParticipants] = useState(Array.from(
+  const [remoteParticipants, setRemotePerticipants] = useState(Array.from(
     room.participants.values(),
   ));
 
-  console.log(remoteParticipants, setRemoteParticipants);
+  const [participants, setParticipants] = useRecoilState(participantsAtom);
+
+  const addParticipantToStore = (participant: any) => {
+    if (participants.find((p:any) => p.identity === participant.identity)) {
+      return "";
+    }
+    const newParticipants = [...participants];
+    newParticipants.push({ identity: participant.identity });
+    setParticipants(_.uniqBy(newParticipants, "identity"));
+    return "";
+  };
+
+  const removeParticipantFromStore = (participant:any) => {
+    const filteredParticipants = participants.filter(
+      (p:any) => p.identity !== participant.identity,
+    );
+    setParticipants(_.uniqBy(filteredParticipants, "identity"));
+  };
+
+  const addParticipant = (participant:any) => {
+    addParticipantToStore(participant);
+    setRemotePerticipants((prev) => _.uniqBy([...prev, participant], "identity"));
+  };
+
+  const removeParticipant = (participant:any) => {
+    removeParticipantFromStore(participant);
+    setRemotePerticipants((prev) => prev.filter((p:any) => p.identity !== participant.identity));
+  };
+
+  useEffect(() => {
+    remoteParticipants.forEach((participant) => {
+      addParticipant(participant);
+    });
+
+    room.on("participantConnected", (participant:any) => addParticipant(participant));
+
+    room.on("participantDisconnected", (participant:any) => {
+      removeParticipant(participant);
+    });
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div>
-      <Participant participant={room.localParticipant} room={room} />
+      <Participant participant={room.localParticipant} room={room} isLocal />
+
+      {remoteParticipants.map(
+        (p:any) => <Participant key={p.identity} participant={p} room={room} />,
+      )}
     </div>
   );
 };
