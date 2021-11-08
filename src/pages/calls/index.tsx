f01fd12625093f736726c43c1ecb7ee71a7fbdc8
Mahmudul Alam <expelmahmud@gmail.com>
2021-11-08 10:04:42 +0600

    add: uuid

--- src/pages/calls/index.tsx
+++ src/pages/calls/index.tsx
@@ -1,19 +1,40 @@
+import { useEffect } from "react";
 import { Flex } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import { v4 } from "uuid";
 // import { useLocation } from "react-router-dom";
 import Overlay from "../../components/call/Overlay";
 
-// import SideBar from "../../components/call/SideBar";
+import useTwilioToken from "../../hooks/twilio/useTwilioToken";
+import { twilioTokenAtom } from "../../components/call/atoms";
+
+import SideBar from "../../components/call/SideBar";
 
 export default function VideoCalls() {
-  // const { search } = useLocation();
+  const {
+    mutate: getAccessToken,
+    // error,
+    isLoading,
+    // isIdle,
+    // isSuccess,
+    // isError,
+  } = useTwilioToken();
+
+  useEffect(() => {
+    getAccessToken({ room_name: "test", id: v4() });
+    // eslint-disable-next-line
+  }, []);
+
+  const token = useRecoilValue(twilioTokenAtom);
+
+  console.log(token);
 
   return (
     <Flex minHeight="100vh">
-      {/* <SideBar /> */}
+      <SideBar />
 
-      <Overlay />
+      {isLoading && <Overlay />}
 
-      <p>Preparing video conference....</p>
     </Flex>
   );
 }
