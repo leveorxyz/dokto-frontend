eff8e9708a2f290e568fd72dd613e98c35f4f7af
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-11 05:06:32 +0600

    Update DoctorSection style

--- src/components/home/DoctorSection.tsx
+++ src/components/home/DoctorSection.tsx
@@ -16,23 +16,25 @@ const items = [
 const DoctorSection = () => (
   <Box>
     <Flex direction="column" alignItems="center">
-      <Box w="100%" d="flex" flexDir="column" alignItems="top" m="4" fontSize="xl" fontWeight="semibold" color="#170041">
-        <Box px={[0, 0, 6, 6, 6]}>
+      <Box w="100%" d="flex" flexDir="column" alignItems="top" m="4" fontSize="lg">
+        <Box px={[0, 0, 6, 6, 6]} color="primary.dark" fontSize="2xl" fontWeight="600">
           Your virtual office without the overhead.
         </Box>
-        <Box px={[0, 0, 6, 6, 6]} mt="4">
-          Our platform enables licensed medical practitioners like you to see patients from virtually anywhere. Many of our providers range from Primary Care Providers, Dermatologists, Counselors, Therapists, Psychologists, Psychiatrists, Home Health Caregivers, Nurse Practitioners and more.
-        </Box>
-        <Box px={[0, 0, 6, 6, 6]} mt="4">
-          Become a part of the global network that is revolutionizing healthcare. We take the guesswork out of your consultancy booking system and give you the ability to provide your patients with unlimited access to quality and affordable healthcare. And all of this is possible without leaving your home or office - through the convenience of chat, SMS, email or video conference.
-        </Box>
-        <Box px={[0, 0, 6, 6, 6]} mt="4">
+        {
+          [
+            "Our platform enables licensed medical practitioners like you to see patients from virtually anywhere. Many of our providers range from Primary Care Providers, Dermatologists, Counselors, Therapists, Psychologists, Psychiatrists, Home Health Caregivers, Nurse Practitioners and more.",
+            "Become a part of the global network that is revolutionizing healthcare. We take the guesswork out of your consultancy booking system and give you the ability to provide your patients with unlimited access to quality and affordable healthcare. And all of this is possible without leaving your home or office - through the convenience of chat, SMS, email or video conference.",
+          ].map(
+            (item) => <Box key={item} px={[0, 0, 8, 8, 8]} color="gray.500" mt="4">{item}</Box>,
+          )
+        }
+        <Box px={[0, 0, 6, 6, 6]} fontWeight="600" color="primary.dark" mt={4}>
           As a Dokto provider you can:
         </Box>
-        <Box px={[0, 0, 8, 8, 8]} mt="4">
+        <Box px={[0, 0, 8, 8, 8]} mt={2}>
           <PinkBulletList listData={items} />
         </Box>
-        <Box px={[0, 0, 6, 6, 6]} mt="4">
+        <Box px={[0, 0, 6, 6, 6]} fontWeight="600" color="primary.dark" mt="4">
           We are proud to work with only licensed and board-certified experts.
         </Box>
       </Box>
