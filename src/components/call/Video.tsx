a2622a1a7d00b85391e723a1bf28cbcfb1dcbd67
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-11 18:59:59 +0600

    Fix video component

--- src/components/call/Video.tsx
+++ src/components/call/Video.tsx
@@ -8,10 +8,16 @@ export default function Video({
   videoRef,
 }: PropTypes) {
   return (
-    <Box as="video" w="100%" h="100%" position="relative" rounded="lg">
+    <Box w="100%" h="100%" position="relative" rounded="lg">
       {/* these videos wouldn't have captions so it's safe to disable the caption tracks */}
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video ref={videoRef} autoPlay />
+      <video
+        ref={videoRef}
+        autoPlay
+        style={{
+          borderRadius: "15px",
+        }}
+      />
     </Box>
   );
 }
