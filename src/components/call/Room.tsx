6d74df339148c3c3208335da46af20ca61fc56ee
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-11 19:05:30 +0600

    Remove unnecessary component

--- src/components/call/Room.tsx
+++ src/components/call/Room.tsx
@@ -1,10 +1,32 @@
-import VideoSection from "./VideoSection";
+import { useState, useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { Room as RoomType } from "twilio-video";
+import { twilioTokenAtom } from "./atoms";
+import Videos from "./Videos";
+import TwilioUtils from "./utils/TwilioUtils";
+import LoadingPage from "../common/fallback/LoadingPage";
 
-const Room = () => (
-  <div>
-    {/* Video Section */}
-    <VideoSection />
-  </div>
-);
+const Room = () => {
+  const [room, setRoom] = useState<RoomType | null>(null);
+  const { token, roomName } = useRecoilValue(twilioTokenAtom);
+
+  useEffect(() => {
+    if (token && roomName) {
+      TwilioUtils.connectToRoom(token, roomName).then((currentRoom) => {
+        setRoom(currentRoom);
+      });
+    }
+  }, [token, roomName]);
+
+  if (!room) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <>
+      <Videos key={room.sid} room={room} />
+    </>
+  );
+};
 
 export default Room;
