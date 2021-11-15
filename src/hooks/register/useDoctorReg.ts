65848becb3b3a8c28f10794a42348dba5068ae1a
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-15 12:40:25 +0600

    Remove login in after registration

--- src/hooks/register/useDoctorReg.ts
+++ src/hooks/register/useDoctorReg.ts
@@ -1,10 +1,8 @@
 import { useContext } from "react";
 import { useQuery } from "react-query";
 import { AxiosInstance } from "axios";
-import { useSetRecoilState } from "recoil";
 
 import { AxiosContext } from "../../contexts/AxiosContext";
-import authAtom from "../../atoms/auth.atom";
 
 const registerDoctor = async (axios: AxiosInstance, data: any) => axios.post("user/doctor-signup/", data)
   .then(({ data: { result } }) => Promise.resolve(result))
@@ -12,33 +10,10 @@ const registerDoctor = async (axios: AxiosInstance, data: any) => axios.post("us
 
 export default function useDoctorReg(data: any) {
   const axios = useContext<AxiosInstance | null>(AxiosContext);
-  const setAuthState = useSetRecoilState(authAtom);
 
   return useQuery(
     ["doctor_reg", data],
     () => registerDoctor(axios as AxiosInstance, data),
-    {
-      retry: 2,
-      onSuccess: ({
-        token,
-        id,
-        email,
-        profile_photo: profilePhoto,
-        user_type: userType,
-        full_name: fullName,
-      }) => {
-        setAuthState({
-          isLoggedIn: true,
-          user: {
-            id,
-            email,
-            token,
-            profilePhoto,
-            userType,
-            fullName,
-          },
-        });
-      },
-    },
+    { retry: 2 },
   );
 }
