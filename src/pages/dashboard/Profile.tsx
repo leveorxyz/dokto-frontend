100575415132c4c5eb2b484872b045fbd18b7310
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-15 16:27:56 +0600

    Integrate useProfile hook

--- src/pages/dashboard/Profile.tsx
+++ src/pages/dashboard/Profile.tsx
@@ -1,5 +1,41 @@
+import {
+  Heading,
+  Flex,
+  Box,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+
+import { SpacedContainer } from "../../components/common/Containers";
+import doctorProfileAtom from "../../atoms/doctorProfile";
+import useProfile from "../../hooks/profile/useProfile";
+import LoadingPage from "../../components/common/fallback/LoadingPage";
+
 export default function Profile() {
+  const doctorProfile = useRecoilValue(doctorProfileAtom);
+  const { isLoading } = useProfile("doctor", doctorProfileAtom);
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
   return (
-    <h1>Profile</h1>
+    <SpacedContainer py={6}>
+      <Heading as="h2" fontSize="xl" fontWeight={500} color="primary.dark">Profile</Heading>
+      <Flex wrap="wrap" py={12} alignItems="flex-start">
+        <Image src={doctorProfile?.profilePhoto} width="12rem" fit="contain" mr={6} />
+        <Box>
+          <Heading as="h3" fontSize="3xl" fontWeight={700} color="primary.dark">{doctorProfile?.fullName}</Heading>
+          <Heading as="h4" fontSize="xl" fontWeight={400} color="brand.darkPink" mb={3}>{doctorProfile?.qualificationSuffix}</Heading>
+          <Box color="gray.500" mb={3}>
+            No reviews yet.
+          </Box>
+          <Text fontSize="lg" color="gray.500">
+            {doctorProfile?.professionalBio}
+          </Text>
+        </Box>
+      </Flex>
+    </SpacedContainer>
   );
 }
