3f8107f2b44ee462ab34fc46fe3299f5bf77143e
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-15 12:23:18 +0600

    remove unused mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ import "swiper/swiper-vars.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
 
-import { ThemeProvider, createMuiTheme } from "@material-ui/core";
-import { purple } from "@material-ui/core/colors";
 import Routes from "./router";
 import NavBar from "./components/nav/Bar";
 import Footer from "./components/common/footer/Footer";
@@ -21,15 +19,6 @@ import theme from "./components/ExtendedTheme";
 import AxiosContextProvider from "./contexts/AxiosContext";
 
 const queryClient = new QueryClient();
-
-const muiTheme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#ababab",
-    },
-    secondary: purple,
-  },
-});
 
 function AppContent() {
   return (
@@ -44,19 +33,17 @@ function AppContent() {
 export default function App() {
   return (
     <RecoilRoot>
-      <ThemeProvider theme={muiTheme}>
-        <ChakraProvider theme={theme}>
-          <Router>
-            <AxiosContextProvider>
-              <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <AxiosContextProvider>
+            <QueryClientProvider client={queryClient}>
 
-                <AppContent />
+              <AppContent />
 
-              </QueryClientProvider>
-            </AxiosContextProvider>
-          </Router>
-        </ChakraProvider>
-      </ThemeProvider>
+            </QueryClientProvider>
+          </AxiosContextProvider>
+        </Router>
+      </ChakraProvider>
     </RecoilRoot>
   );
 }
