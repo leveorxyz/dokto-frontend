2a7a45a42581916bad01c8f643b47256f0698468
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-15 16:22:31 +0600

    Implement useProfile hook

---
+++ src/hooks/profile/useProfile.ts
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { useQuery } from "react-query";
+import { AxiosInstance } from "axios";
+import { useSetRecoilState, RecoilState } from "recoil";
+
+import { AxiosContext } from "../../contexts/AxiosContext";
+import { DoctorProfile } from "../../atoms/doctorProfile";
+import { toCamelCaseObj } from "../../utils/toCamelCase";
+
+const getProfile = async (axios: AxiosInstance, userType: string) => axios.get(`dashboard/${userType}/profile/`)
+  .then(({ data: { result } }) => Promise.resolve(result))
+  .catch(({ response: { data: response } }) => Promise.reject(response));
+
+export default function useProfile(
+  userType: string,
+  profileAtom: RecoilState<DoctorProfile | null>,
+) {
+  const axios = useContext<AxiosInstance | null>(AxiosContext);
+  const setProfile = useSetRecoilState(profileAtom);
+
+  return useQuery(
+    ["profile", { userType, profileAtom }],
+    () => getProfile(axios as AxiosInstance, userType),
+    {
+      retry: false,
+      onSuccess: (data) => {
+        setProfile(toCamelCaseObj(data) as DoctorProfile);
+      },
+    },
+  );
+}
