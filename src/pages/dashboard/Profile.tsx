f7c7916ddaedd7b2e96bcf3a22baf74a2f5ad1d0
Afia Anjum <afiatamanna06@gmail.com>
2021-11-20 20:32:28 +0600

    Add profile right section cards

--- src/pages/dashboard/Profile.tsx
+++ src/pages/dashboard/Profile.tsx
@@ -12,6 +12,7 @@ import ProfileTabs from "../../components/profile/doctor/ProfileTabs";
 import doctorProfileAtom from "../../atoms/doctorProfile";
 import useProfile from "../../hooks/profile/useProfile";
 import LoadingPage from "../../components/common/fallback/LoadingPage";
+import ProfileRightRow from "../../components/profile/doctor/ProfileRightRow";
 
 export default function Profile() {
   const doctorProfile = useRecoilValue(doctorProfileAtom);
@@ -24,20 +25,23 @@ export default function Profile() {
   return (
     <SpacedContainer py={6}>
       <Heading as="h2" fontSize="xl" fontWeight={500} color="primary.dark">Profile</Heading>
-      <Flex wrap="wrap" py={12} alignItems="flex-start">
-        <Image src={doctorProfile?.profile_photo} width="12rem" fit="contain" mr={6} />
-        <Box>
-          <Heading as="h3" fontSize="3xl" fontWeight={700} color="primary.dark">{doctorProfile?.full_name}</Heading>
-          <Heading as="h4" fontSize="xl" fontWeight={400} color="brand.darkPink" mb={3}>{doctorProfile?.qualification_suffix}</Heading>
-          <Box color="gray.500" mb={3}>
-            No reviews yet.
+      <Flex direction="row">
+        <Flex wrap="wrap" py={12} alignItems="flex-start" direction="column" w="90%" pr={[0, 0, 20, 20]}>
+          <Image src={doctorProfile?.profile_photo} width="12rem" fit="contain" mr={6} />
+          <Box w="100%">
+            <Heading as="h3" fontSize="3xl" fontWeight={700} color="primary.dark">{doctorProfile?.full_name}</Heading>
+            <Heading as="h4" fontSize="xl" fontWeight={400} color="brand.darkPink" mb={3}>{doctorProfile?.qualification_suffix}</Heading>
+            <Box color="gray.500" mb={3}>
+              No reviews yet.
+            </Box>
+            <Text fontSize="lg" color="gray.500">
+              {doctorProfile?.professional_bio}
+            </Text>
           </Box>
-          <Text fontSize="lg" color="gray.500">
-            {doctorProfile?.professional_bio}
-          </Text>
-        </Box>
+          <ProfileTabs />
+        </Flex>
+        <ProfileRightRow />
       </Flex>
-      <ProfileTabs />
     </SpacedContainer>
   );
 }
