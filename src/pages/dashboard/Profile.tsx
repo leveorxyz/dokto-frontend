1b481128421dfc7eaecb2e7bbea5db8b607a93b3
Afia Anjum <afiatamanna06@gmail.com>
2021-11-20 23:47:33 +0600

    Add profile fixes in dashboard

--- src/pages/dashboard/Profile.tsx
+++ src/pages/dashboard/Profile.tsx
@@ -12,9 +12,6 @@ import ProfileTabs from "../../components/profile/doctor/ProfileTabs";
 import doctorProfileAtom from "../../atoms/doctorProfile";
 import useProfile from "../../hooks/profile/useProfile";
 import LoadingPage from "../../components/common/fallback/LoadingPage";
-import ProfileRightRow from "../../components/profile/doctor/ProfileRightRow";
-import BrandButton from "../../components/common/buttons/BrandButton";
-import WhiteButton from "../../components/common/buttons/WhiteButton";
 
 export default function Profile() {
   const doctorProfile = useRecoilValue(doctorProfileAtom);
@@ -28,9 +25,9 @@ export default function Profile() {
     <SpacedContainer py={6}>
       <Heading as="h2" fontSize="xl" fontWeight={500} color="primary.dark">Profile</Heading>
       <Flex direction="row">
-        <Flex wrap="wrap" py={12} alignItems="flex-start" direction="column" w="90%" pr={[0, 0, 20, 20]}>
+        <Flex wrap="wrap" py={12} alignItems="flex-start" direction="row" w="90%" pr={[0, 0, 20, 20]}>
           <Image src={doctorProfile?.profile_photo} width="12rem" fit="contain" mr={6} />
-          <Box w="100%" mb="10">
+          <Box w="100%" mb="10" d="flex" flexDir="column">
             <Heading as="h3" fontSize="3xl" fontWeight={700} color="primary.dark">{doctorProfile?.full_name}</Heading>
             <Heading as="h4" fontSize="xl" fontWeight={400} color="brand.darkPink" mb={3}>{doctorProfile?.qualification_suffix}</Heading>
             <Box color="gray.500" mb={3}>
@@ -39,14 +36,13 @@ export default function Profile() {
             <Text fontSize="lg" color="gray.500">
               {doctorProfile?.professional_bio}
             </Text>
-            <Flex direction="row">
+            {/* <Flex direction="row" mt="6">
               <BrandButton>Add Feedback</BrandButton>
               <WhiteButton ml="6">Book Now</WhiteButton>
-            </Flex>
+  </Flex> */}
           </Box>
           <ProfileTabs />
         </Flex>
-        <ProfileRightRow />
       </Flex>
     </SpacedContainer>
   );
