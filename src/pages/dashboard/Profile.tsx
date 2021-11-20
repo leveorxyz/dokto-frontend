ce9c5814c15ad61693e2afebfa62a51fb48a1b97
Afia Anjum <afiatamanna06@gmail.com>
2021-11-20 22:40:01 +0600

    Add profile review's bottom buttons

--- src/pages/dashboard/Profile.tsx
+++ src/pages/dashboard/Profile.tsx
@@ -13,6 +13,8 @@ import doctorProfileAtom from "../../atoms/doctorProfile";
 import useProfile from "../../hooks/profile/useProfile";
 import LoadingPage from "../../components/common/fallback/LoadingPage";
 import ProfileRightRow from "../../components/profile/doctor/ProfileRightRow";
+import BrandButton from "../../components/common/buttons/BrandButton";
+import WhiteButton from "../../components/common/buttons/WhiteButton";
 
 export default function Profile() {
   const doctorProfile = useRecoilValue(doctorProfileAtom);
@@ -28,7 +30,7 @@ export default function Profile() {
       <Flex direction="row">
         <Flex wrap="wrap" py={12} alignItems="flex-start" direction="column" w="90%" pr={[0, 0, 20, 20]}>
           <Image src={doctorProfile?.profile_photo} width="12rem" fit="contain" mr={6} />
-          <Box w="100%">
+          <Box w="100%" mb="10">
             <Heading as="h3" fontSize="3xl" fontWeight={700} color="primary.dark">{doctorProfile?.full_name}</Heading>
             <Heading as="h4" fontSize="xl" fontWeight={400} color="brand.darkPink" mb={3}>{doctorProfile?.qualification_suffix}</Heading>
             <Box color="gray.500" mb={3}>
@@ -37,6 +39,10 @@ export default function Profile() {
             <Text fontSize="lg" color="gray.500">
               {doctorProfile?.professional_bio}
             </Text>
+            <Flex direction="row">
+              <BrandButton>Add Feedback</BrandButton>
+              <WhiteButton ml="6">Book Now</WhiteButton>
+            </Flex>
           </Box>
           <ProfileTabs />
         </Flex>
