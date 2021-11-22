200b3d0647a3336ca206bb911305317ee6e52e88
Afia Anjum <afiatamanna06@gmail.com>
2021-11-22 18:18:01 +0600

    Add uselocation hook

--- src/components/nav/Sidebar.tsx
+++ src/components/nav/Sidebar.tsx
@@ -3,26 +3,30 @@ import {
   Flex,
   Box,
 } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
 
 import routes from "../../router/routes";
 
-const Sidebar = () => (
-  <div>
-    <Flex d="flex" direction="column" h="100%" justifyContent="space-around">
-      {routes
-        .filter((route) => route.showInDashboard)
-        .map((route) => (
-          <Box
-            key={route.displayName}
-            p={6}
-            cursor="pointer"
-            _hover={{ bgColor: "brand.darkPink", color: "white" }}
-          >
-            {route.displayName}
-          </Box>
-        ))}
-    </Flex>
-  </div>
-);
-
+const Sidebar = () => {
+  const location = useLocation();
+  return (
+    <div>
+      <Flex d="flex" direction="column" h="100%" justifyContent="space-around">
+        {routes
+          .filter((route) => route.showInDashboard)
+          .map((route) => (
+            <Box
+              key={route.displayName}
+              p={6}
+              color={(location.pathname === route.path) ? "brand.darkPink" : "black"}
+              cursor="pointer"
+              _hover={{ bgColor: "brand.darkPink", color: "white" }}
+            >
+              {route.displayName}
+            </Box>
+          ))}
+      </Flex>
+    </div>
+  );
+};
 export default Sidebar;
