4439a4f1a9ae244fd33c68b9bfd324307f5be527
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-22 18:34:48 +0600

    Update navbar styling

--- src/components/nav/Sidebar.tsx
+++ src/components/nav/Sidebar.tsx
@@ -1,32 +1,45 @@
-import React from "react";
 import {
   Flex,
   Box,
+  Link as ChakraLink,
 } from "@chakra-ui/react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 import routes from "../../router/routes";
 
 const Sidebar = () => {
   const location = useLocation();
   return (
-    <div>
-      <Flex d="flex" direction="column" h="100%" justifyContent="space-around">
-        {routes
-          .filter((route) => route.showInDashboard)
-          .map((route) => (
+    <Flex
+      justifyContent="flex-start"
+      direction="column"
+      minHeight="100vh"
+      overflowY="scroll"
+      overflowX="hidden"
+      bg="gray.100"
+    >
+      {routes
+        .filter((route) => route.showInDashboard)
+        .map((route) => (
+          <ChakraLink as={Link} to={route.path} key={route.path}>
             <Box
-              key={route.displayName}
               p={6}
-              color={(location.pathname === route.path) ? "brand.darkPink" : "black"}
+              m={2}
+              bg={location.pathname === route.path ? "brand.darkPink" : "transparent"}
+              color={location.pathname === route.path ? "white" : "black"}
               cursor="pointer"
-              _hover={{ bgColor: "brand.darkPink", color: "white" }}
+              _hover={{
+                bg: location.pathname === route.path ? "brand.darkPink" : "white",
+                color: location.pathname === route.path ? "white" : "brand.darkPink",
+                textDecoration: "none",
+              }}
+              rounded="lg"
             >
               {route.displayName}
             </Box>
-          ))}
-      </Flex>
-    </div>
+          </ChakraLink>
+        ))}
+    </Flex>
   );
 };
 export default Sidebar;
