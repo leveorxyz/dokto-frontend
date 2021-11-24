d1a4ee6a9a0e1ef60d0e14d4205ad4bfc370885c
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-25 03:27:53 +0600

    Add PatientTable to page

--- src/pages/dashboard/Patients.tsx
+++ src/pages/dashboard/Patients.tsx
@@ -1,3 +1,35 @@
+import { useMemo } from "react";
+import { useRecoilValue } from "recoil";
+import { useTable, useSortBy, usePagination } from "react-table";
+
+import patientsAtom from "../../atoms/ehr/patients";
+import { columns } from "../../data/PatientsData";
+import PatientTable from "../../components/patients/Table";
+
 export default function Patients() {
-  return <></>;
+  const patientsData = useRecoilValue(patientsAtom);
+  const memoizedColumns = useMemo(() => columns, []);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns: memoizedColumns,
+    data: patientsData,
+  },
+  useSortBy, usePagination);
+
+  return (
+    <PatientTable
+      {...{
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      }}
+    />
+  );
 }
