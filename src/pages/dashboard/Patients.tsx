ccd1e187b8fd293b256cb6c8fdb413078c04a564
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-11-25 15:55:05 +0600

    Implement search

--- src/pages/dashboard/Patients.tsx
+++ src/pages/dashboard/Patients.tsx
@@ -1,18 +1,42 @@
 import { useState, useMemo } from "react";
 import { useRecoilValue } from "recoil";
 import { useTable, useSortBy, usePagination } from "react-table";
-import { useDisclosure } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Input,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { matchSorter } from "match-sorter";
 
 import patientsAtom, { Patient } from "../../atoms/ehr/patients";
 import { columns } from "../../data/PatientsData";
 import PatientTable from "../../components/patients/Table";
 import PatientModal from "../../components/patients/OverviewModal";
+import { SpacedContainer } from "../../components/common/Containers";
 
 export default function Patients() {
   const patientsData = useRecoilValue(patientsAtom);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [currentPatient, setCurrentPatient] = useState<Patient | null>(null);
+  const [searchParam, setSearchParam] = useState<string | null>(null);
+
   const memoizedColumns = useMemo(() => columns, []);
+  const memoizedPatientsData = useMemo(
+    () => {
+      if (!searchParam) return patientsData;
+      return matchSorter(
+        patientsData,
+        searchParam,
+        {
+          keys: ["name", "address", "phoneNo", "reasonOfVisit", "status"],
+        },
+      );
+    }, [searchParam, patientsData],
+  );
+
   const showModal = (patient: Patient) => {
     setCurrentPatient(patient);
     onOpen();
@@ -30,12 +54,35 @@ export default function Patients() {
     prepareRow,
   } = useTable({
     columns: memoizedColumns,
-    data: patientsData,
+    data: memoizedPatientsData,
   },
   useSortBy, usePagination);
 
   return (
-    <>
+    <SpacedContainer py={12}>
+      <Flex justifyContent="space-between" pb={6}>
+        <Box>
+          <Heading as="h1" fontSize="3xl">
+            Patients
+          </Heading>
+          <Text fontSize="lg" color="gray.500">
+            List of all patients
+          </Text>
+        </Box>
+
+        <Box>
+          <Text fontSize="lg">
+            Search Patients
+          </Text>
+          <Input
+            placeholder="Search"
+            value={searchParam ?? ""}
+            variant="filled"
+            onChange={(e) => setSearchParam(e.target.value)}
+          />
+        </Box>
+      </Flex>
+
       <PatientTable
         {...{
           getTableProps,
@@ -51,6 +98,6 @@ export default function Patients() {
         isOpen={isOpen}
         patient={currentPatient}
       />
-    </>
+    </SpacedContainer>
   );
 }
