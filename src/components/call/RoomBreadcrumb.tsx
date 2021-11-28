461356c36e4568ce1bebd2415eb2e21cc04d0b85
Mahmudul Alam <expelmahmud@gmail.com>
2021-11-28 19:26:53 +0600

    add: chat icon

--- src/components/call/RoomBreadcrumb.tsx
+++ src/components/call/RoomBreadcrumb.tsx
@@ -2,25 +2,31 @@ import {
   Breadcrumb, BreadcrumbItem, Box, Icon,
 } from "@chakra-ui/react";
 import { FaUserMd } from "react-icons/fa";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
 
 type PropTypes ={
   doctor: string,
   isPatient: boolean
 }
 const RoomBreadcrumb = ({ doctor, isPatient }:PropTypes) => (
-  <Breadcrumb position="fixed" left={isPatient ? "17rem" : "32rem"} top="3">
-    <BreadcrumbItem>
-      <Box textTransform="capitalize">
-        <Icon as={FaUserMd} />
-        {" "}
-        {doctor}
-      </Box>
-    </BreadcrumbItem>
+  <>
+    <Breadcrumb position="fixed" left={isPatient ? "17rem" : "32rem"} top="3">
+      <BreadcrumbItem>
+        <Box textTransform="capitalize">
+          <Icon as={FaUserMd} />
+          {" "}
+          {doctor}
+        </Box>
+      </BreadcrumbItem>
 
-    <BreadcrumbItem>
-      <Box>Telehealth Appointment</Box>
-    </BreadcrumbItem>
-  </Breadcrumb>
+      <BreadcrumbItem>
+        <Box>Telehealth Appointment</Box>
+      </BreadcrumbItem>
+    </Breadcrumb>
+    <Box position="fixed" right="10" top="3">
+      <Icon as={IoChatboxEllipsesOutline} fontSize="30" _hover={{ color: "purple" }} />
+    </Box>
+  </>
 );
 
 export default RoomBreadcrumb;
