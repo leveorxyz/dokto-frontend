0dd9d7e6677251175f673b0e0300c004562d7e5b
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-02 16:06:59 +0600

    F*** go back

--- src/components/call/RoomBreadcrumb.tsx
+++ src/components/call/RoomBreadcrumb.tsx
@@ -1,5 +1,5 @@
 import {
-  Breadcrumb, BreadcrumbItem, Box, Icon, Flex,
+  Breadcrumb, BreadcrumbItem, Box, Icon,
 } from "@chakra-ui/react";
 import { FaUserMd } from "react-icons/fa";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
@@ -9,17 +9,7 @@ type PropTypes ={
   isPatient: boolean
 }
 const RoomBreadcrumb = ({ doctor, isPatient }:PropTypes) => (
-  <Flex
-    justifyContent="space-between"
-    top={0}
-    right={0}
-    m={2}
-    p={2}
-    width="98%"
-    rounded="lg"
-    bgColor="gray.700"
-    color="gray.100"
-  >
+  <>
     <Breadcrumb position="fixed" left={isPatient ? "17rem" : "32rem"} top="3">
       <BreadcrumbItem>
         <Box textTransform="capitalize">
@@ -32,10 +22,10 @@ const RoomBreadcrumb = ({ doctor, isPatient }:PropTypes) => (
         <Box>Telehealth Appointment</Box>
       </BreadcrumbItem>
     </Breadcrumb>
-    <Box>
+    <Box position="fixed" right="10" top="3">
       <Icon as={IoChatboxEllipsesOutline} fontSize="30" _hover={{ color: "purple" }} />
     </Box>
-  </Flex>
+  </>
 );
 
 export default RoomBreadcrumb;
