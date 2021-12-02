e6d4c0084660923f27bd7d91e94ba47a05e6b380
Mahmudul Alam <expelmahmud@gmail.com>
2021-12-03 00:40:59 +0600

    add: conditional translation

--- src/components/call/ActionButtons.tsx
+++ src/components/call/ActionButtons.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import { Room } from "twilio-video";
+import { useRecoilValue } from "recoil";
 import CameraButton from "./CameraButton";
 import LeaveRoomButton from "./LeaveRoomButton";
 import MicButton from "./MicButton";
 import ScreenShareButton from "./ScreenShareButton";
+import authAtom from "../../atoms/auth.atom";
 
 type PropTypes = {
   room: Room,
@@ -14,18 +16,22 @@ type PropTypes = {
 
 const ActionButtons = ({
   room, setScreenShareStream, isScreenSharingActive, setIsScreenSharingActive,
-}: PropTypes) => (
-  <Box pos="fixed" bottom="10" left="50%" transform="translateX(50%)">
-    <CameraButton room={room} />
-    <MicButton room={room} />
-    <ScreenShareButton
-      room={room}
-      setScreenShareStream={setScreenShareStream}
-      isScreenSharingActive={isScreenSharingActive}
-      setIsScreenSharingActive={setIsScreenSharingActive}
-    />
-    <LeaveRoomButton room={room} />
-  </Box>
-);
+}: PropTypes) => {
+  const authState = useRecoilValue(authAtom);
+
+  return (
+    <Box pos="fixed" bottom="10" left="50%" transform={`translateX(${authState?.user?.userType === "DOCTOR" ? 50 : 0}%)`}>
+      <CameraButton room={room} />
+      <MicButton room={room} />
+      <ScreenShareButton
+        room={room}
+        setScreenShareStream={setScreenShareStream}
+        isScreenSharingActive={isScreenSharingActive}
+        setIsScreenSharingActive={setIsScreenSharingActive}
+      />
+      <LeaveRoomButton room={room} />
+    </Box>
+  );
+};
 
 export default ActionButtons;
