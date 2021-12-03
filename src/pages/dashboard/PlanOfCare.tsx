4e9c5da38ac49a10876183ad189b36f30d9ad52b
BanerjeeProdipta <prodiptaj@live.com>
2021-12-03 16:50:21 +0600

    Added Rich Text Editor on plan of care page

--- src/pages/dashboard/PlanOfCare.tsx
+++ src/pages/dashboard/PlanOfCare.tsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import {
   Heading,
   Box,
 } from "@chakra-ui/react";
-
+import ReactQuill from "react-quill";
 import { SpacedContainer } from "../../components/common/Containers";
 import doctorProfileAtom from "../../atoms/doctorProfile";
 import useProfile from "../../hooks/profile/useProfile";
 import LoadingPage from "../../components/common/fallback/LoadingPage";
-import PlanOfCareForm from "../../components/forms/plan-of-care";
+import "react-quill/dist/quill.snow.css";
 
 export default function PlanOfCare() {
   const { isLoading } = useProfile("doctor", doctorProfileAtom);
+  const [value, setValue] = useState("");
 
   if (isLoading) {
     return <LoadingPage />;
@@ -26,8 +28,10 @@ export default function PlanOfCare() {
         boxShadow="0 10px 30px 0 rgb(205 204 219 / 25%)"
       >
         <Heading as="h2" fontSize="xl" fontWeight={500} color="primary.dark" mb="5" background="primary.light" p="2">Plan of Care</Heading>
-        {/* <Divider mb="5" /> */}
-        <PlanOfCareForm />
+
+        <Box p="4">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </Box>
 
       </Box>
     </SpacedContainer>
