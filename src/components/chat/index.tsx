e5565c893dca8dcdd7b126e5f0c6e42b85f538a5
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-04 22:47:54 +0600

    Create chat component

---
+++ src/components/chat/index.tsx
@@ -0,0 +1,94 @@
+import { useMemo, useState } from "react";
+import {
+  Box, Flex, Input, Button,
+} from "@chakra-ui/react";
+
+type ParticipantTextType = {
+  text: string;
+  isLocal: boolean;
+}
+
+type StyleType = {
+  mr?: string;
+  ml?: string;
+  bgColor?: string;
+  color?: string;
+}
+
+const ParticipantText = ({ text, isLocal }: ParticipantTextType) => {
+  const styles = useMemo(() => {
+    const style: StyleType = {};
+    if (isLocal) {
+      style.ml = "auto";
+      style.bgColor = "primary.dark";
+      style.color = "white";
+    } else {
+      style.mr = "auto";
+      style.bgColor = "white";
+      style.color = "primary.dark";
+    }
+  }, [isLocal]);
+
+  return (
+    <Box
+      width="70%"
+      rounded="lg"
+      py={1}
+      {...styles}
+    >
+      {text}
+    </Box>
+  );
+};
+
+const dummyTexts = [
+  {
+    id: 1,
+    text: "This is a text sent by the LOCAL participant",
+    isLocal: true,
+  },
+  {
+    id: 2,
+    text: "This is a text sent by the REMOTE participant",
+    isLocal: false,
+  },
+];
+
+export default function Chat() {
+  const [text, setText] = useState("");
+  const [texts, setTexts] = useState(dummyTexts);
+
+  return (
+    <Box bgColor="gray.100">
+      <Box overflowY="scroll">
+        {texts.map(({ text: textContent, id, isLocal }) => (
+          <ParticipantText
+            key={id}
+            text={textContent}
+            isLocal={isLocal}
+          />
+        ))}
+      </Box>
+
+      <Flex wrap="nowrap" p={2} h="15rem">
+        <Input
+          placeholder="Type a message..."
+          flexGrow={1}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button
+          onClick={() => setTexts(
+            (prev) => [...prev, {
+              id: prev.length + 1,
+              text,
+              isLocal: true,
+            }],
+          )}
+        >
+          Send
+        </Button>
+      </Flex>
+    </Box>
+  );
+}
