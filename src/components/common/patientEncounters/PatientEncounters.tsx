340a961fd7672884c3e32a2c1d96b4ae42531414
BanerjeeProdipta <prodiptaj@live.com>
2021-12-04 12:37:12 +0600

    Styling added Encounter Patients Table

--- src/components/common/patientEncounters/PatientEncounters.tsx
+++ src/components/common/patientEncounters/PatientEncounters.tsx
@@ -4,11 +4,9 @@ import { useTable, useSortBy, usePagination } from "react-table";
 import patientEncountersAtom from "../../../atoms/ehr/patientEncounters";
 import { columns } from "../../../data/PatientEncountersData";
 import EncountersTable from "./Table";
-import { SpacedContainer } from "../Containers";
 
 export default function Encounters() {
   const data = useRecoilValue(patientEncountersAtom);
-  console.log(data);
   const memoizedColumns = useMemo(() => columns, []);
   const memoizedData = useMemo(
     () => data, [data],
