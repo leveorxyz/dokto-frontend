02d3a4b2d549523e866c6ca712d105d8851e555b
Afia Anjum <afiatamanna06@gmail.com>
2021-12-05 07:11:56 +0600

    Add update experience hook

--- src/components/forms/experience-education/steps/Submit.tsx
+++ src/components/forms/experience-education/steps/Submit.tsx
@@ -3,8 +3,8 @@ import { Navigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 import Loading from "../../../common/fallback/LoadingPage";
-import { stepAtom } from "../atoms";
-import usePatientReg from "../../../../hooks/register/usePatientReg";
+import stepAtom from "../../../../atoms/educationExperience.atom";
+import useUpdateEducationExperience from "../../../../hooks/profile-settings/updateEducationExperiences";
 import MessagePage from "../../../common/fallback/MessagePage";
 
 export default function Submit() {
@@ -13,43 +13,29 @@ export default function Submit() {
   const data = useMemo(() => ({
     ...Object.keys(stepData).reduce(
       (prev, curr) => {
-        if (curr === "contact_no-prefix") {
-          return { ...prev, contact_no: stepData[curr] + prev.contact_no ?? "" };
-        }
-        if (curr === "contact_no-value") {
-          return { ...prev, contact_no: (prev.contact_no ?? "") + stepData[curr] };
-        }
-        if (curr === "first_name") {
-          return { ...prev, full_name: `${stepData[curr]} ${prev.full_name.trim()}` };
-        }
-        if (curr === "last_name") {
-          return { ...prev, full_name: `${prev.full_name.trim()} ${stepData[curr]}` };
-        }
         if (!stepData[curr] || stepData[curr] === "") return prev;
         return { ...prev, [curr]: stepData[curr] };
       },
       {
-        contact_no: "",
-        full_name: "",
       },
     ),
   }), [stepData]);
 
   const {
     error, isError, isSuccess, isFetching,
-  } = usePatientReg(data);
+  } = useUpdateEducationExperience(data);
 
   if (isFetching) {
     return <Loading />;
   }
 
   if (isSuccess) {
-    return <MessagePage status="success" title="Success!" message="You have successfully registered to Dokto! Please check your email and verify your email to log in to Dokto" />;
+    return <MessagePage status="success" title="Success!" message="Successfully updated education and experience data" />;
   }
 
   if (isError) {
     return <MessagePage status="error" title="Oops!" message={(error as any).message} />;
   }
 
-  return <Navigate to="/" />;
+  return <Navigate to="/profile-settings" />;
 }
