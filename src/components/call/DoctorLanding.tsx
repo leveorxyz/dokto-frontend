4e2b42e8fb5b93bd680be1992169e0236b598cd5
Mahmudul Alam <expelmahmud@gmail.com>
2021-12-09 01:01:13 +0600

    add: consulting room & waiting room card

--- src/components/call/DoctorLanding.tsx
+++ src/components/call/DoctorLanding.tsx
@@ -1,8 +1,10 @@
 import { useState, useMemo } from "react";
 import {
-  Flex, Box, Text,
+  Flex, Box, Text, Image,
 } from "@chakra-ui/react";
 import BrandButton from "../common/buttons/BrandButton";
+import ConsultingImage from "../../static/doctor-consulting.svg";
+import EditRoom from "../../static/waiting-room.svg";
 
 type PropTypes = {
   roomName: string;
@@ -25,12 +27,11 @@ const DoctorLanding = ({ roomName, userName }:PropTypes) => {
   };
 
   return (
-    <Flex w="100%" backgroundColor="white">
+    <Box w="100%" backgroundColor="white" paddingLeft="5%">
       <Box
         boxShadow="0px 4px 4px rgba(0, 0, 0, 0.07)"
         width="60%"
         height="243px"
-        marginLeft="5%"
         marginTop="50px"
         borderRadius="10px"
         border="1px solid #dddddd"
@@ -48,8 +49,37 @@ const DoctorLanding = ({ roomName, userName }:PropTypes) => {
           </BrandButton>
         </Flex>
       </Box>
-      <Box paddingTop="6rem" textAlign="center" />
-    </Flex>
+      <Box width="60%" marginTop="30px">
+        <Flex justifyContent="space-between">
+          <Box
+            width="60%"
+            height="243px"
+            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.07)"
+            border="1px solid #dddddd"
+            borderRadius="10px"
+            textAlign="center"
+            _hover={{ backgroundColor: "#81F9EC" }}
+          >
+            <Image src={ConsultingImage} margin="0 auto" />
+            <Text fontSize="20px" fontWeight="500">Enter Your Consulting Room</Text>
+          </Box>
+          <Box
+            width="35%"
+            height="243px"
+            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.07)"
+            border="1px solid #dddddd"
+            borderRadius="10px"
+            marginLeft="10px"
+            textAlign="center"
+            paddingTop="3.5rem"
+            _hover={{ backgroundColor: "#A42BAD" }}
+          >
+            <Image src={EditRoom} margin="0 auto" marginTop="10%" />
+            <Text fontSize="20px" fontWeight="500" marginTop="10px">Edit Waiting Room</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
   );
 };
 
