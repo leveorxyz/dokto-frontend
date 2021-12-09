5fd60267539a22d770737a5f20457c6d56157e10
Mahmudul Alam <expelmahmud@gmail.com>
2021-12-09 18:22:42 +0600

    add: Back to dashboard link

--- src/pages/calls/edit-room.tsx
+++ src/pages/calls/edit-room.tsx
@@ -1,6 +1,17 @@
+import { Box, Text, Icon } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { IoArrowBackOutline } from "react-icons/io5";
+
 const EditRoom = () => (
   <div>
-    Edit Room
+    <Box w="100%" paddingLeft="5%" paddingTop="35px">
+      <Link to="/calls">
+        <Text display="flex" alignItems="center" fontSize="18px" color="#7002C7" fontWeight="500">
+          <Icon as={IoArrowBackOutline} marginRight="5px" />
+          Back to dashboard
+        </Text>
+      </Link>
+    </Box>
   </div>
 );
 
