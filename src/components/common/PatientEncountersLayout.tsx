2a5ddfc9052eab5c97caf2de641a58a0a9fea6e7
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-09 09:39:44 +0600

    Add Back button to PatientEncountersLayout

--- src/components/common/PatientEncountersLayout.tsx
+++ src/components/common/PatientEncountersLayout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { SpacedContainer } from "./Containers";
+
+import BackButton from "./buttons/BackButton";
 
 interface props{
   children: React.ReactNode;
@@ -9,9 +11,17 @@ interface props{
 const PatientEncountersLayout :React.FC<props> = ({ children }) => (
   <Box backgroundColor="#f7f7fc" minH="100vh">
     <SpacedContainer mx={12}>
-      <Heading fontSize="xl" fontWeight="600" color="brand.darkPink" py="8">
-        PATIENT ENCOUNTERS
-      </Heading>
+      <Flex py="8" alignItems="center">
+        <Heading fontSize="xl" fontWeight="600" color="brand.darkPink" pr={4}>
+          PATIENT ENCOUNTERS
+        </Heading>
+        <BackButton
+          size="sm"
+          text="Close Tab"
+          bg="rgba(255, 92, 0, 0.1)"
+          color="rgba(255, 92, 0, 1)"
+        />
+      </Flex>
       <Box pb={12}>{children}</Box>
     </SpacedContainer>
   </Box>
