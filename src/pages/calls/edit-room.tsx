211df64beecf209f59f324b05cafd7921a0460ed
Mahmudul Alam <expelmahmud@gmail.com>
2021-12-09 18:47:55 +0600

    add: Revert & Save button

--- src/pages/calls/edit-room.tsx
+++ src/pages/calls/edit-room.tsx
@@ -1,18 +1,45 @@
-import { Box, Text, Icon } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import { IoArrowBackOutline } from "react-icons/io5";
+import { Box, Text, Flex } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import BacktoDashboard from "../../components/call/edit-room/BacktoDashboard";
+import authAtom from "../../atoms/auth.atom";
+import BrandButton from "../../components/common/buttons/BrandButton";
+import WhiteButton from "../../components/common/buttons/WhiteButton";
 
-const EditRoom = () => (
-  <div>
-    <Box w="100%" paddingLeft="5%" paddingTop="35px">
-      <Link to="/calls">
-        <Text display="flex" alignItems="center" fontSize="18px" color="#7002C7" fontWeight="500">
-          <Icon as={IoArrowBackOutline} marginRight="5px" />
-          Back to dashboard
-        </Text>
-      </Link>
-    </Box>
-  </div>
-);
+const EditRoom = () => {
+  const authState = useRecoilValue(authAtom);
+  return (
+    <div>
+      <Box w="100%" paddingLeft="5%" paddingTop="35px">
+        <BacktoDashboard />
+
+        <Box
+          backgroundColor="#fff"
+          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.07)"
+          width="60%"
+          height="243px"
+          marginTop="15px"
+          borderRadius="10px"
+          border="1px solid #dddddd"
+          padding="50px"
+          paddingLeft="25px"
+          paddingRight="25px"
+        >
+          <Text fontSize="25px">{`Waiting Rooms of ${authState?.user?.fullName}`}</Text>
+          <Text color="#9A9AB0" size="18px" marginTop="5px">Customize your Waiting Rooms</Text>
+          <Flex marginTop="45px" justifyContent="flex-end" alignItems="center">
+            <Box marginRight="20px">
+              <WhiteButton>
+                Revert to Default
+              </WhiteButton>
+            </Box>
+            <BrandButton width="100px">
+              Save
+            </BrandButton>
+          </Flex>
+        </Box>
+      </Box>
+    </div>
+  );
+};
 
 export default EditRoom;
