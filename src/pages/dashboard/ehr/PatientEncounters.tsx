873bb53e5ac2afca55087242cf4929a0e746d101
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-09 07:59:05 +0600

    Update PatientEncounters UI

--- src/pages/dashboard/ehr/PatientEncounters.tsx
+++ src/pages/dashboard/ehr/PatientEncounters.tsx
@@ -1,16 +1,85 @@
-import { Box, Button } from "@chakra-ui/react";
-import React from "react";
+import { Box, Flex, Link as ChakraLink } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { FiEdit } from "react-icons/fi";
+
+import PatientEncountersLayout from "../../../components/common/PatientEncountersLayout";
+
+const encountersData = [
+  {
+    url: "/patients/assessment-and-diagnosis",
+    title: "Assessment and Diagnosis",
+  },
+  {
+    url: "/patients/plan-of-care",
+    title: "Plan of Care",
+  },
+  {
+    url: "/patients/patient-procedure",
+    title: "Patient Procedure",
+  },
+  {
+    url: "/patients/functional-and-cognitive-status",
+    title: "Functional And Cognitive Status",
+  },
+  {
+    url: "/patients/chief-complaints-and-HPI",
+    title: "Chief Complaints and HPI",
+  },
+  {
+    url: "/patients/vitals",
+    title: "Vitals",
+  },
+];
 
 const PatientEncounters = () => (
-  <Box>
-    <Link to="/patients/assessment-and-diagnosis"><Button m="2rem">Assessment and Diagnosis</Button></Link>
-    <Link to="/patients/plan-of-care"><Button m="2rem">Plan of Care</Button></Link>
-    <Link to="/patients/patient-procedure"><Button m="2rem">Patient Procedure</Button></Link>
-    <Link to="/patients/functional-and-cognitive-status"><Button m="2rem">Functional And Cognitive Status</Button></Link>
-    <Link to="/patients/chief-complaints-and-HPI"><Button m="2rem">Chief Complaints and HPI</Button></Link>
-    <Link to="/patients/vitals"><Button m="2rem">Vitals</Button></Link>
-  </Box>
+  <PatientEncountersLayout>
+    <Box bgColor="white" pb={6}>
+      {encountersData.map(
+        ({ url, title }) => (
+          <Box
+            key={url}
+            mb={6}
+          >
+            <ChakraLink
+              as={Link}
+              to={url}
+              _hover={{ textDecoration: "none" }}
+            >
+              <Flex
+                justifyContent="space-between"
+                alignItems="center"
+                bgColor="primary.light"
+                py={1}
+                px={6}
+                fontWeight="semibold"
+              >
+                <Box>{title}</Box>
+                <Flex alignItems="center" color="brand.darkPurple">
+                  <Box
+                    as={FiEdit}
+                    size="1.2rem"
+                    mr={2}
+                  />
+                  <Box>Edit</Box>
+                </Flex>
+              </Flex>
+            </ChakraLink>
+
+            <Box
+              minH="8rem"
+              borderBottomRadius="lg"
+              borderWidth={1}
+              borderTopWidth={0}
+              borderColor="brand.darkPurple"
+              p={6}
+            >
+              No result found
+            </Box>
+          </Box>
+        ),
+      )}
+    </Box>
+  </PatientEncountersLayout>
 );
 
 export default PatientEncounters;
