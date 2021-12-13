afce7a4250a21ffb3c8cfd900bef097b5670c254
BanerjeeProdipta <prodiptaj@live.com>
2021-12-13 15:54:55 +0600

    Tobacco/ Smoking Use

--- src/data/SocialHistory.tsx
+++ src/data/SocialHistory.tsx
@@ -17,4 +17,38 @@ export const higherEducationOptions = [
 
 ];
 
-export default { homeEnvironmentOptions, higherEducationOptions };
+export const tobaccoStatusOptions = [
+  { label: "Current everyday smoker", value: "Current everyday smoker" },
+  { label: "Former smoker", value: "Former smoker" },
+  { label: "Never smoker", value: "Never smoker" },
+  { label: "Smoker, current status unknown", value: "Smoker, current status unknown" },
+  { label: "Unknown if ever smoked", value: "Unknown if ever smoked" },
+  { label: "Heavy tobacco smoker", value: "Heavy tobacco smoker" },
+  { label: "Light tobacco smoker", value: "Light tobacco smoker" },
+
+];
+
+export const tobaccoTypeOptions = [
+  { label: "Cigarette", value: "Cigarette" },
+  { label: "Cigar", value: "Cigar" },
+  { label: "Chewing tobacco", value: "Chewing tobacco" },
+  { label: "Cigrate & Chewing", value: "Cigrate & Chewing" },
+
+];
+
+export const tobaccoPacksPerDayOptions = [
+  { label: "Less than half pack", value: "Less than half pack" },
+  { label: "Half pack", value: "Half pack" },
+  { label: "Less than 1 pack", value: "Less than 1 pack" },
+  { label: "1 pack", value: "1 pack" },
+  { label: "More than 1 pack", value: "More than 1 pack" },
+
+];
+
+export default {
+  homeEnvironmentOptions,
+  higherEducationOptions,
+  tobaccoStatusOptions,
+  tobaccoTypeOptions,
+  tobaccoPacksPerDayOptions,
+};
--- src/pages/dashboard/ehr/SocialHistory.tsx
+++ src/pages/dashboard/ehr/SocialHistory.tsx
@@ -6,10 +6,14 @@ import {
   FormErrorMessage,
   FormLabel,
   Select,
+  Button,
+  Input,
 } from "@chakra-ui/react";
-
 import { useForm } from "react-hook-form";
-import { higherEducationOptions, homeEnvironmentOptions } from "../../../data/SocialHistory";
+import {
+  higherEducationOptions, homeEnvironmentOptions,
+  tobaccoPacksPerDayOptions, tobaccoStatusOptions, tobaccoTypeOptions,
+} from "../../../data/SocialHistory";
 import CustomAccordion from "../../../components/common/CustomAccordion";
 import PatientEncountersLayout from "../../../components/common/PatientEncountersLayout";
 import doctorProfileAtom from "../../../atoms/doctorProfile";
@@ -19,6 +23,15 @@ import LoadingPage from "../../../components/common/fallback/LoadingPage";
 interface IAssessmentCreate{
   home_environment:string,
   highest_education:string,
+  occupation:string,
+  children:string,
+  marital_status:string,
+  status:string,
+  tobacco_status:string,
+  tobacco_type:string,
+  tobacco_packs_per_day:string,
+  tobacco_start_date:string,
+  tobacco_cessation:string
 }
 
 export default function SocialHistory() {
@@ -32,6 +45,8 @@ export default function SocialHistory() {
 
   const onSubmit = handleSubmit(async (data) => {
     console.log(data);
+
+    // useSocialHistoryAdd(data);
   });
 
   if (isLoading) {
@@ -95,10 +110,158 @@ export default function SocialHistory() {
                   </FormControl>
 
                 </Flex>
-
+                <FormControl isInvalid={!!errors.occupation} pb={6}>
+                  <FormLabel htmlFor="occupation" color="primary.dark">Occupation</FormLabel>
+                  <Input
+                    {...register("occupation", {
+                      required: {
+                        value: true,
+                        message: "This field is required",
+                      },
+                    })}
+                    name="occupation"
+                  />
+                  <FormErrorMessage>
+                    {errors.occupation && errors.occupation.message}
+                  </FormErrorMessage>
+                </FormControl>
+                <Flex experimental_spaceX={8} pb={6}>
+                  <FormControl isInvalid={!!errors.children}>
+                    <FormLabel htmlFor="children" color="primary.dark">Children</FormLabel>
+                    <Input
+                      {...register("children", {
+                        required: {
+                          value: true,
+                          message: "This field is required",
+                        },
+                      })}
+                      name="children"
+                    />
+                    <FormErrorMessage>
+                      {errors.children && errors.children.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <FormControl isInvalid={!!errors.marital_status} pb={6}>
+                    <FormLabel htmlFor="marital_status" color="primary.dark">Marital Status</FormLabel>
+                    <Input
+                      {...register("marital_status", {
+                        required: {
+                          value: true,
+                          message: "This field is required",
+                        },
+                      })}
+                      name="marital_status"
+                    />
+                    <FormErrorMessage>
+                      {errors.marital_status && errors.marital_status.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </Flex>
               </Box>
             </CustomAccordion>
 
+            {/* TOBACCO  */}
+            <CustomAccordion title="Tobacco/ Smoking Use">
+              <Box>
+                <Flex experimental_spaceX={8}>
+                  <FormControl pb={6}>
+                    <FormLabel htmlFor="tobacco_status" color="brand.dark">Status</FormLabel>
+                    <Select
+                      placeholder="Select"
+                      {...register("tobacco_status")}
+                    >
+                      {tobaccoStatusOptions.map(({ label, value }) => (
+                        <option
+                          key={value}
+                          value={value}
+                        >
+                          {label}
+                        </option>
+                      ))}
+                    </Select>
+                    <FormErrorMessage>
+                      {errors.tobacco_status && errors.tobacco_status.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <FormControl pb={6}>
+                    <FormLabel htmlFor="tobacco_type" color="brand.dark">Type</FormLabel>
+                    <Select
+                      placeholder="Select"
+                      {...register("tobacco_type")}
+                    >
+                      {tobaccoTypeOptions.map(({ label, value }) => (
+                        <option
+                          key={value}
+                          value={value}
+                        >
+                          {label}
+                        </option>
+                      ))}
+                    </Select>
+                    <FormErrorMessage>
+                      {errors.tobacco_type && errors.tobacco_type.message}
+                    </FormErrorMessage>
+                  </FormControl>
+
+                </Flex>
+                <FormControl pb={6}>
+                  <FormLabel htmlFor="tobacco_packs_per_day" color="brand.dark">Pack(s)/ Day</FormLabel>
+                  <Select
+                    placeholder="Select"
+                    {...register("tobacco_packs_per_day")}
+                  >
+                    {tobaccoPacksPerDayOptions.map(({ label, value }) => (
+                      <option
+                        key={value}
+                        value={value}
+                      >
+                        {label}
+                      </option>
+                    ))}
+                  </Select>
+                  <FormErrorMessage>
+                    {errors.tobacco_packs_per_day && errors.tobacco_packs_per_day.message}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <Flex experimental_spaceX={8} pb={6}>
+                  <FormControl isInvalid={!!errors.tobacco_start_date}>
+                    <FormLabel htmlFor="tobacco_start_date" color="primary.dark">Year Started</FormLabel>
+                    <Input
+                      {...register("tobacco_start_date", {
+                        required: {
+                          value: true,
+                          message: "This field is required",
+                        },
+                      })}
+                      name="tobacco_start_date"
+                    />
+                    <FormErrorMessage>
+                      {errors.tobacco_start_date && errors.tobacco_start_date.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <FormControl isInvalid={!!errors.tobacco_cessation} pb={6}>
+                    <FormLabel htmlFor="tobacco_cessation" color="primary.dark">Year of Cessation</FormLabel>
+                    <Input
+                      {...register("tobacco_cessation", {
+                        required: {
+                          value: true,
+                          message: "This field is required",
+                        },
+                      })}
+                      name="tobacco_cessation"
+                    />
+                    <FormErrorMessage>
+                      {errors.tobacco_cessation && errors.tobacco_cessation.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </Flex>
+              </Box>
+            </CustomAccordion>
+            <Flex justifyContent="end">
+              <Button>Cancel</Button>
+              <Button type="submit" ml={2} colorScheme="purple">Save</Button>
+            </Flex>
           </form>
 
         </Box>
