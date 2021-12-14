cc879ef4814cbc98b524917c5c23e24db5f5a524
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-14 08:09:34 +0600

    Update Doctor Dashboard rendering

--- src/components/dashboard/DoctorDashboard.tsx
+++ src/components/dashboard/DoctorDashboard.tsx
@@ -9,37 +9,43 @@ import { Row1, Row2 } from "./DashboardData/Doctor";
 
 const DashboardHome = () => (
   <SpacedContainer mx={4}>
-    <Box py="4" color="#11142D" fontSize="2xl" fontWeight="semibold" mx={6}>Dashboard</Box>
-    <WelcomeCard
-      description="Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor."
-      readMoreLink="#"
-      img={null}
-    />
-    <Flex wrap="wrap">
-      {Row1
-        .map(({
-          title, path, icon,
-        }) => (
-          <Card
-            key={path}
-            icon={<Image src={icon} />}
-            title={title}
-            path={path}
-          />
-        ))}
-    </Flex>
-    <Flex wrap="wrap">
-      {Row2
-        .map(({
-          title, path, icon,
-        }) => (
-          <Card
-            key={path}
-            icon={<Image src={icon} />}
-            title={title}
-            path={path}
-          />
-        ))}
+    <Flex>
+      <Box
+        w="75%"
+      >
+        <Box py="4" color="#11142D" fontSize="2xl" fontWeight="semibold" mx={6}>Dashboard</Box>
+        <WelcomeCard
+          description="Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor."
+          readMoreLink="#"
+          img={null}
+        />
+        <Flex wrap="wrap">
+          {Row1
+            .map(({
+              title, path, icon,
+            }) => (
+              <Card
+                key={path}
+                icon={<Image src={icon} />}
+                title={title}
+                path={path}
+              />
+            ))}
+        </Flex>
+        <Flex wrap="wrap">
+          {Row2
+            .map(({
+              title, path, icon,
+            }) => (
+              <Card
+                key={path}
+                icon={<Image src={icon} />}
+                title={title}
+                path={path}
+              />
+            ))}
+        </Flex>
+      </Box>
     </Flex>
   </SpacedContainer>
 );
