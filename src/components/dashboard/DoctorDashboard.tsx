a6294bea7a7027a0c32bda83fb16dfdc0958dfae
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-15 01:59:49 +0600

    Reformat DoctorDashboard

--- src/components/dashboard/DoctorDashboard.tsx
+++ src/components/dashboard/DoctorDashboard.tsx
@@ -8,46 +8,57 @@ import WelcomeCard from "./WelcomeCard";
 import MyProfile from "./MyProfile";
 import { row1, row2 } from "./DashboardData/Doctor";
 
+const Row1 = () => (
+  <Flex wrap="wrap">
+    {row1
+      .map(({
+        title, path, icon,
+      }) => (
+        <Card
+          key={path}
+          icon={<Image src={icon} />}
+          title={title}
+          path={path}
+        />
+      ))}
+  </Flex>
+);
+
+const Row2 = () => (
+  <Flex wrap="wrap">
+    {row2
+      .map(({
+        title, path, icon,
+      }) => (
+        <Card
+          key={path}
+          icon={<Image src={icon} />}
+          title={title}
+          path={path}
+        />
+      ))}
+  </Flex>
+);
+
 const DashboardHome = () => (
   <SpacedContainer mx={4}>
     <Box py="4" color="#11142D" fontSize="2xl" fontWeight="semibold" mx={6}>Dashboard</Box>
-    <Flex>
+    <Flex wrap={["wrap-reverse", "wrap-reverse", "wrap-reverse", "wrap", "wrap"]}>
+
       <Box w="85%">
         <WelcomeCard
           description="Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor."
           readMoreLink="#"
           img={null}
         />
-        <Flex wrap="wrap">
-          {row1
-            .map(({
-              title, path, icon,
-            }) => (
-              <Card
-                key={path}
-                icon={<Image src={icon} />}
-                title={title}
-                path={path}
-              />
-            ))}
-        </Flex>
-        <Flex wrap="wrap">
-          {row2
-            .map(({
-              title, path, icon,
-            }) => (
-              <Card
-                key={path}
-                icon={<Image src={icon} />}
-                title={title}
-                path={path}
-              />
-            ))}
-        </Flex>
+        <Row1 />
+        <Row2 />
       </Box>
-      <Box flexGrow={1}>
+
+      <Box flexGrow={1} px={3}>
         <MyProfile />
       </Box>
+
     </Flex>
   </SpacedContainer>
 );
