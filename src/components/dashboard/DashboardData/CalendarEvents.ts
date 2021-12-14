ac54e00b22515d45e8a454e03341caedb99502a2
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-15 04:24:23 +0600

    Create dummy calendar events

---
+++ src/components/dashboard/DashboardData/CalendarEvents.ts
@@ -0,0 +1,41 @@
+export type CalendarEvent = {
+  id: string | number;
+  type: string;
+  doctorName: string;
+  date: string;
+  time: string;
+  bgColor: string;
+  textColor: string;
+};
+
+export const Events: CalendarEvent[] = [
+  {
+    id: 1,
+    type: "Dentist",
+    doctorName: "Dr. John Doe",
+    date: "2020-12-01",
+    time: "10:00 PM",
+    bgColor: "#16D090",
+    textColor: "white",
+  },
+  {
+    id: 2,
+    type: "Radiography",
+    doctorName: "Dr. John Doe",
+    date: "2020-12-05",
+    time: "10:00 PM",
+    bgColor: "#FF6760",
+    textColor: "white",
+  },
+  {
+    id: 3,
+    type: "Orphopedic",
+    doctorName: "Dr. John Doe",
+    date: "2020-12-20",
+    time: "10:00 PM",
+    bgColor: "#FAC032",
+    textColor: "black",
+  },
+];
+
+export default Events;
