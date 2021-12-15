6ae9e7dd3a7313f4b1fa3c4ce75b4de40234ac46
Prodipta Banerjee <prodiptaj@live.com>
2021-12-15 13:17:15 +0600

    route and form

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -275,7 +275,7 @@ const routes: RouteType[] = [
   },
   {
     displayName: "Vitals",
-    path: "/patients/encounters-data/vitals",
+    path: "/patients/encounters-data/vitals/:id",
     component: <Vitals />,
     isProtected: true,
     showInNavbar: false,
