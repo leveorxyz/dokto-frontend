dee1288dfd4f065fc48d0b5b0fc5d0081dd287b6
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-15 08:48:04 +0600

    Implement uploading photo and converting to base64

--- src/components/profile/doctor/profile_settings/ProfileDetails.tsx
+++ src/components/profile/doctor/profile_settings/ProfileDetails.tsx
@@ -1,33 +1,66 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import {
-  Box, Heading, Flex, Image, Button,
+  Box,
+  Heading,
+  Flex,
+  Image,
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
 } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import ProfileDetailsForm from "../../../forms/profile-details";
 import doctorProfileAtom from "../../../../atoms/doctorProfile";
 import editIcon from "../../../../static/edit_image.svg";
+import getBase64 from "../../../../utils/getBase64";
 
 const ProfileDetails = () => {
+  const [profilePhotoUrl, setProfilePhotoUrl] = useState<string | null>(null);
+  const [profilePhotoBase64, setProfilePhotoBase64] = useState<string | null>(null);
+
   const doctorProfile = useRecoilValue(doctorProfileAtom);
+
+  useEffect(() => setProfilePhotoUrl(doctorProfile?.profile_photo ?? null), [doctorProfile]);
+
   return (
     <Box>
       <Heading as="h2" fontWeight="600" fontSize="2xl" color="brand.dark">Your Details</Heading>
       <Flex direction="row" alignItems="center" py="12" px="6">
-        <Image src={doctorProfile?.profile_photo} fit="cover" boxSize="10rem" borderRadius="50%" mr="10" />
-        <Button
-          bgColor="brand.darkPink"
-          color="white"
-          p="2.5"
-          rounded="lg"
-          h="10%"
-          d="flex"
-          alignItems="center"
-          _hover={{ opacity: ".85" }}
-          _focus={{ outline: "none", opacity: ".85" }}
-        >
-          <Box mr="3">Edit Image</Box>
-          <Image src={editIcon} />
-        </Button>
+        <Image src={profilePhotoUrl ?? ""} fit="cover" boxSize="10rem" borderRadius="50%" mr="10" />
+        <form>
+          <FormControl>
+            <Input
+              id="upload-photo"
+              type="file"
+              visibility="hidden"
+              accept="image/*"
+              onChange={(e) => {
+                const file = e.target.files?.item(0);
+                setProfilePhotoUrl((prev) => (file ? URL.createObjectURL(file) : prev));
+                getBase64(file as File)
+                  .then((base64) => setProfilePhotoBase64(base64 as string ?? null));
+              }}
+            />
+            <FormLabel htmlFor="upload-photo">
+              <Button
+                bgColor="brand.darkPink"
+                color="white"
+                p="2.5"
+                rounded="lg"
+                h="10%"
+                d="flex"
+                alignItems="center"
+                pointerEvents="none"
+                _hover={{ opacity: ".85" }}
+                _focus={{ outline: "none", opacity: ".85" }}
+              >
+                <Box mr="3">Edit Image</Box>
+                <Image src={editIcon} />
+              </Button>
+            </FormLabel>
+          </FormControl>
+        </form>
       </Flex>
       <ProfileDetailsForm />
     </Box>
