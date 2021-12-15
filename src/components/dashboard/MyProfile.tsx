d25537a31d3f8b103227f2b6113588fbea296ac2
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-16 05:19:52 +0600

    Use clip-path for making profile bg half

--- src/components/dashboard/MyProfile.tsx
+++ src/components/dashboard/MyProfile.tsx
@@ -21,13 +21,31 @@ export default function MyProfile() {
       boxShadow="0 10px 30px 0 rgb(205 204 219 / 25%)"
     >
       <Heading as="h3" fontSize="xl" fontWeight="semibold" mb={6}>My Profile</Heading>
-      <Box p="3px" bgGradient="linear(45deg, #2D96EB, #1DAE83)" rounded="full" w="max-content" h="max-content">
+      <Box
+        p="3px"
+        rounded="full"
+        w="max-content"
+        h="max-content"
+        position="relative"
+        zIndex={0}
+      >
         <Avatar
           p="3px"
           bg="white"
           size="xl"
           name={authState.user?.fullName}
           src={authState.user?.profilePhoto}
+        />
+        <Box
+          position="absolute"
+          top="-1px"
+          left="-1px"
+          h="calc(100% + 2px)"
+          w="calc(100% + 2px)"
+          rounded="full"
+          zIndex={-1}
+          bgGradient="linear(to-b, #2D96EB, #1DAE83)"
+          clipPath="polygon(0% 0%,50% 0%,50% 100%,0% 100%);"
         />
       </Box>
     </Flex>
