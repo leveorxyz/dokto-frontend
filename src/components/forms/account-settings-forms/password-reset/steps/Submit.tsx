df8f476551b714f600c6097c014e28db4df0df08
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-21 03:11:56 +0600

    Integrate password update page

--- src/components/forms/account-settings-forms/password-reset/steps/Submit.tsx
+++ src/components/forms/account-settings-forms/password-reset/steps/Submit.tsx
@@ -1,29 +1,27 @@
 import { useMemo } from "react";
-import { Navigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 import Loading from "../../../../common/fallback/LoadingPage";
-import stepAtom from "../../../../../atoms/updateAccountSettings.atom";
-import useUpdatePasswordReset from "../../../../../hooks/account-settings/updatePasswordReset";
+import stepAtom from "../../../../../atoms/dashboard/accountSettings.atom";
+import { useUpdateAccountSettings } from "../../../../../hooks/account-settings/useAccountSettings";
 import MessagePage from "../../../../common/fallback/MessagePage";
 
 export default function Submit() {
   const stepData = useRecoilValue<any>(stepAtom);
 
   const data = useMemo(() => ({
-    ...Object.keys(stepData).reduce(
+    ...Object.keys(stepData || {})?.reduce(
       (prev, curr) => {
         if (!stepData[curr] || stepData[curr] === "") return prev;
         return { ...prev, [curr]: stepData[curr] };
       },
-      {
-      },
+      {},
     ),
   }), [stepData]);
 
   const {
     error, isError, isSuccess, isFetching,
-  } = useUpdatePasswordReset(data);
+  } = useUpdateAccountSettings(data);
 
   if (isFetching) {
     return <Loading />;
@@ -37,5 +35,5 @@ export default function Submit() {
     return <MessagePage status="error" title="Oops!" message={(error as any).message} />;
   }
 
-  return <Navigate to="/account-settings" />;
+  return <></>;
 }
