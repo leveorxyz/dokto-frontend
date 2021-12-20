34ef428d1f663162fceb8f8eacb679225e71bfe0
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-21 03:04:40 +0600

    Implement useUpdateAccountSettingsHook

--- src/hooks/account-settings/useAccountSettings.ts
+++ src/hooks/account-settings/useAccountSettings.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { AxiosInstance } from "axios";
 import { useSetRecoilState, RecoilState } from "recoil";
 
@@ -9,9 +9,24 @@ const getAccountSettings = async (axios: AxiosInstance) => axios.get("dashboard/
   .then(({ data: { result } }) => Promise.resolve(result))
   .catch(({ response: { data: response } }) => Promise.reject(response));
 
-export default function useAccountSettings(
-  dataAtom: RecoilState<unknown>,
-) {
+const updateAccountSettings = async (axios: AxiosInstance, data: unknown) => axios.put("dashboard/account-settings/", data)
+  .then(({ data: { result } }) => Promise.resolve(result))
+  .catch(({ response: { data: response } }) => Promise.reject(response));
+
+export const useUpdateAccountSettings = (data: unknown) => {
+  const axios = useContext<AxiosInstance | null>(AxiosContext);
+  const queryClient = useQueryClient();
+
+  return useQuery(
+    ["account-settings", axios, data],
+    () => updateAccountSettings(axios as AxiosInstance, data),
+    {
+      onSuccess: () => queryClient.invalidateQueries("get/account-settings"),
+    },
+  );
+};
+
+export default function useAccountSettings(dataAtom: RecoilState<unknown>) {
   const axios = useContext<AxiosInstance | null>(AxiosContext);
   const setData = useSetRecoilState(dataAtom);
 
