7c653faa650349f53fc994f8a88430890f3eaf66
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-21 06:29:42 +0600

    Remove data loading from ProfileSettingsTabs

--- src/components/profile/doctor/profile_settings/ProfileSettingsTabs.tsx
+++ src/components/profile/doctor/profile_settings/ProfileSettingsTabs.tsx
@@ -2,10 +2,6 @@ import {
   Tabs, TabList, TabPanels, Tab, TabPanel,
 } from "@chakra-ui/react";
 
-import doctorProfile from "../../../../atoms/dashboard/doctorProfileSettings.atom";
-import useProfile from "../../../../hooks/profile/useProfile";
-import MessagePage from "../../../common/fallback/MessagePage";
-import LoadingPage from "../../../common/fallback/LoadingPage";
 import ExperienceEducation from "./ExperienceEducation";
 import ProfileDetails from "./ProfileDetails";
 import ProfessionalProfile from "./ProfessionalProfile";
@@ -13,81 +9,66 @@ import Insurances from "./Insurances";
 
 export default function ProfileSettingsTabs() {
   const tabs = ["Personal Details", "Experience & Education", "Professional Profile", "Insurances"];
-  const {
-    isLoading, isError, error, isSuccess,
-  } = useProfile(doctorProfile);
 
-  if (isLoading) {
-    return <LoadingPage />;
-  }
-
-  if (isError) {
-    return <MessagePage status="error" title="Error" message={(error as any).message} />;
-  }
-
-  if (isSuccess) {
-    return (
-      <Tabs
-        align="start"
-        variant="unstyled"
-        w="100%"
-        orientation="vertical"
-        display="grid"
-        gridTemplateColumns="auto 1fr"
+  return (
+    <Tabs
+      align="start"
+      variant="unstyled"
+      w="100%"
+      orientation="vertical"
+      display="grid"
+      gridTemplateColumns="auto 1fr"
+    >
+      <TabList
+        sx={{
+          borderRadius: "0.5rem",
+          border: "1px",
+          borderColor: "brand.dark",
+          height: "max-content",
+          overflow: "hidden",
+        }}
       >
-        <TabList
-          sx={{
-            borderRadius: "0.5rem",
-            border: "1px",
-            borderColor: "brand.dark",
-            height: "max-content",
-            overflow: "hidden",
-          }}
-        >
-          {tabs.map((title, idx) => (
-            <Tab
-              key={title}
-              textAlign="left"
-              justifyContent="start"
-              bgColor="white"
-              color="brand.dark"
-              borderBottomWidth={idx === tabs.length - 1 ? "0" : "1px"}
-              fontSize={["xs", "sm", "md", "lg", "lg"]}
-              fontFamily="sora"
-              borderColor="brand.dark"
-              _hover={{ bgColor: "brand.darkPink", color: "white" }}
-              _selected={{ color: "white", bg: "brand.darkPink" }}
-            >
-              {title}
-            </Tab>
-          ))}
-        </TabList>
+        {tabs.map((title, idx) => (
+          <Tab
+            key={title}
+            textAlign="left"
+            justifyContent="start"
+            bgColor="white"
+            color="brand.dark"
+            borderBottomWidth={idx === tabs.length - 1 ? "0" : "1px"}
+            fontSize={["xs", "sm", "md", "lg", "lg"]}
+            fontFamily="sora"
+            borderColor="brand.dark"
+            _hover={{ bgColor: "brand.darkPink", color: "white" }}
+            _selected={{ color: "white", bg: "brand.darkPink" }}
+          >
+            {title}
+          </Tab>
+        ))}
+      </TabList>
 
-        <TabPanels
-          bgColor="white"
-          borderRadius="lg"
-          boxShadow="0 10px 30px 0 rgb(205 204 219 / 25%)"
-          filter="drop-shadow(15px 70px 90px rgba(164, 43, 173, 0.08))"
-        >
-          <TabPanel>
-            <ProfileDetails />
-          </TabPanel>
+      <TabPanels
+        bgColor="white"
+        borderRadius="lg"
+        boxShadow="0 10px 30px 0 rgb(205 204 219 / 25%)"
+        filter="drop-shadow(15px 70px 90px rgba(164, 43, 173, 0.08))"
+      >
+        <TabPanel>
+          <ProfileDetails />
+        </TabPanel>
 
-          <TabPanel>
-            <ExperienceEducation />
-          </TabPanel>
+        <TabPanel>
+          <ExperienceEducation />
+        </TabPanel>
 
-          <TabPanel>
-            <ProfessionalProfile />
-          </TabPanel>
+        <TabPanel>
+          <ProfessionalProfile />
+        </TabPanel>
 
-          <TabPanel>
-            <Insurances />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    );
-  }
-
-  return <></>;
+        <TabPanel>
+          <Insurances />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
 }
