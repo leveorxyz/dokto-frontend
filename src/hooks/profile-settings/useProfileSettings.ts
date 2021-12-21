e6b710c2bb4d2e590b195c8e326c9483182c86ab
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-21 06:28:44 +0600

    Implement useProfileSettings hooks

---
+++ src/hooks/profile-settings/useProfileSettings.ts
@@ -0,0 +1,58 @@
+import { useContext } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import { AxiosInstance } from "axios";
+import { RecoilState, useSetRecoilState } from "recoil";
+
+import { AxiosContext } from "../../contexts/AxiosContext";
+
+export enum ProfileSettingsURLs {
+  profileDetail = "dashboard/doctor/profile-settings/profile-detail/",
+  educationAndExperience = "dashboard/doctor/profile-settings/education-experience/",
+  insurance = "dashboard/doctor/insurance/",
+  professionalProfile = "dashboard/doctor/professional-profile/",
+}
+
+const getProfileSettins = async (
+  axios: AxiosInstance,
+  url: string,
+) => axios.get(url)
+  .then(({ data: { result } }) => Promise.resolve(result))
+  .catch(({ response: { data: response } }) => Promise.reject(response));
+
+const updateProfileSettings = async (
+  axios: AxiosInstance,
+  url: string,
+  data: unknown,
+) => axios.put(url, data)
+  .then(({ data: { result } }) => Promise.resolve(result))
+  .catch(({ response: { data: response } }) => Promise.reject(response));
+
+export const useUpdateProfileSettings = (url: ProfileSettingsURLs, data: unknown) => {
+  const axios = useContext<AxiosInstance | null>(AxiosContext);
+  const queryClient = useQueryClient();
+
+  return useQuery(
+    [`put/${url}`, url, data],
+    () => updateProfileSettings(axios as AxiosInstance, url, data),
+    {
+      retry: false,
+      staleTime: Infinity,
+      onSuccess: () => queryClient.invalidateQueries(`get/${url}`),
+    },
+  );
+};
+
+export const useProfileSettings = (url: ProfileSettingsURLs, dataAtom: RecoilState<any>) => {
+  const axios = useContext<AxiosInstance | null>(AxiosContext);
+  const setData = useSetRecoilState(dataAtom);
+
+  return useQuery(
+    [`get/${url}`, url],
+    () => getProfileSettins(axios as AxiosInstance, url),
+    {
+      retry: false,
+      staleTime: Infinity,
+      onSuccess: (data) => setData((prev: any) => ({ ...prev, ...data })),
+    },
+  );
+};
