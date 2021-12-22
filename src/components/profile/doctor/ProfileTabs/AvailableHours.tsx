6c4c1a1f7cc74cb4299a0242f8165ee6cd49f822
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-23 02:53:41 +0600

    Integrate Available Hours

--- src/components/profile/doctor/ProfileTabs/AvailableHours.tsx
+++ src/components/profile/doctor/ProfileTabs/AvailableHours.tsx
@@ -1,62 +1,118 @@
+import { useEffect, useState, useMemo } from "react";
 import {
   Box,
   Flex,
   Heading,
 } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
 
-export default function Tab2() {
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-  const times = ["9:00am", "9:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm", "1:00pm", "1:30pm", "2:00pm", "3:00pm"];
+import LoadingPage from "../../../common/fallback/LoadingPage";
+import MessagePage from "../../../common/fallback/MessagePage";
+import AvailableHoursAtom, { AvailableHours } from "../../../../atoms/availableHours.atom";
+import useAvailableHours from "../../../../hooks/profile/useAvailableHours";
 
-  return (
-    <Box>
-      <Heading as="h3" size="md" mb={3}>
-        Available Hours:
-      </Heading>
-      <Flex direction="column">
-        {days
-          .map((day) => (
-            <Flex direction="row" key={day} my="4">
-              <Box color="brand.dark" w="25%">{day}</Box>
-              <Box w="75%">
-                {(day !== "Sunday" && day !== "Wednesday") ? (
-                  times.map((time) => (
+const AvailableHourButton = ({ time }: { time: string }) => (
+  <Box
+    as="button"
+    key={time}
+    border="2px"
+    borderColor="brand.dark"
+    borderRadius="md"
+    w="90px"
+    color="brand.dark"
+    mx="4"
+    my="1"
+    p="1"
+    cursor="pointer"
+    _hover={{ color: "white", bgColor: "brand.darkPink", borderColor: "brand.darkPink" }}
+    _focus={{ color: "white", bgColor: "brand.darkPink" }}
+    _active={{ color: "white", bgColor: "brand.darkPink" }}
+  >
+    {time}
+  </Box>
+);
 
-                    <Box
-                      as="button"
-                      key={time}
-                      border="2px"
-                      borderColor="brand.dark"
-                      borderRadius="md"
-                      w="90px"
-                      color="brand.dark"
-                      mx="4"
-                      my="1"
-                      p="1"
-                      cursor="pointer"
-                      _hover={{ color: "white", bgColor: "brand.darkPink", borderColor: "brand.darkPink" }}
-                      _focus={{ color: "white", bgColor: "brand.darkPink" }}
-                      _active={{ color: "white", bgColor: "brand.darkPink" }}
-                    >
-                      {time}
+type WorkingHours = Record<
+  string,
+  {
+    startTime: string;
+    endTime: string;
+  } | null
+>;
 
-                    </Box>
+export default function AvailableHoursTab() {
+  const weekDays = useMemo(() => ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"], []);
+  const [workingHours, setWorkingHours] = useState<WorkingHours | null>();
 
-                  ))
-                ) : (
-                  <Box
-                    color="red"
-                    mx="4"
-                    p="1"
-                  >
-                    Closed
+  const availableHours = useRecoilValue<AvailableHours[]>(AvailableHoursAtom);
+  const {
+    isSuccess, isError, error,
+  } = useAvailableHours();
 
-                  </Box>
-                )}
-              </Box>
-            </Flex>
-          ))}
-      </Flex>
-    </Box>
+  useEffect(
+    () => {
+      if (isSuccess) {
+        setWorkingHours(
+          weekDays.reduce(
+            (acc, curr) => {
+              const day = availableHours.find((x) => x.day_of_week === curr);
+              if (day) {
+                return ({
+                  ...acc,
+                  [curr]: {
+                    startTime: day.start_time,
+                    endTime: day.end_time,
+                  },
+                });
+              }
+              return ({
+                ...acc,
+                [curr]: null,
+              });
+            },
+            {},
+          ),
+        );
+      }
+    },
+    [isSuccess, availableHours, weekDays],
   );
+
+  if (isError) return <MessagePage status="error" title="Oops!" message={(error as any).message} />;
+
+  if (isSuccess) {
+    return (
+      <Box>
+        <Heading as="h3" size="md" mb={3}>
+          Available Hours:
+        </Heading>
+        <Flex direction="column">
+          {Object.entries(workingHours || {})
+            .map(([day, hours]) => (
+              <Flex direction="row" key={day} my="4">
+                <Box color="brand.dark" w="25%">{day}</Box>
+                <Box w="75%">
+                  {(availableHours.find((item) => item.day_of_week === day)) ? (
+                    <AvailableHourButton
+                      key={`${hours?.startTime} - ${hours?.endTime}`}
+                      time={`${hours?.startTime} - ${hours?.endTime}`}
+                    />
+                  ) : (
+                    <Box
+                      color="red"
+                      mx="4"
+                      p="1"
+                    >
+                      Closed
+                    </Box>
+                  )}
+                </Box>
+              </Flex>
+            ))}
+        </Flex>
+      </Box>
+    );
+  }
+
+  return <LoadingPage />;
 }
