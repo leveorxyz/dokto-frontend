b1731bad175caa037f9cbba073deb708d44e4bf1
Samnan Rahee <samnan.rahee.96@gmail.com>
2021-12-23 20:56:54 +0600

    Remove unused components

--- src/components/profile/doctor/specialties_services/SpecialtiesSection.tsx
+++ src/components/profile/doctor/specialties_services/SpecialtiesSection.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import { Box } from "@chakra-ui/react";
-import SpecialtiesServicesForm from "../../../forms/specialties-services";
 
 const SpecialtiesSection = () => (
   <Box bg="white" borderRadius="0.5rem" p={4}>
-    <SpecialtiesServicesForm />
+    Not Implemented
   </Box>
 );
 
--- src/pages/dashboard/SpecialtiesServices.tsx
+++ src/pages/dashboard/SpecialtiesServices.tsx
@@ -1,39 +1,25 @@
-import React from "react";
 import {
-  Heading, Box,
+  Box,
+  Heading,
   Divider,
 } from "@chakra-ui/react";
 import { SpacedContainer } from "../../components/common/Containers";
 
 import SpecialtiesSection from "../../components/profile/doctor/specialties_services/SpecialtiesSection";
-import doctorProfileAtom from "../../atoms/doctorProfile";
-import useProfile from "../../hooks/profile/useProfile";
-import LoadingPage from "../../components/common/fallback/LoadingPage";
 
-const SpecialtiesServices = () => {
-  const { isLoading } = useProfile(doctorProfileAtom);
-
-  if (isLoading) {
-    return <LoadingPage />;
-  }
-  return (
-    <Box>
-      <SpacedContainer py={10} mx={12}>
-        <Box
-          d="flex"
-          flexDir="column"
-          borderRadius="3xl"
-          borderColor="brand.darkPink"
-          boxShadow="0 10px 30px 0 rgb(205 204 219 / 25%)"
-          filter="drop-shadow(15px 70px 90px rgba(164, 43, 173, 0.08))"
-        >
-          <Heading as="h2" fontSize="xl" fontWeight={500} color="primary.dark" mb="5">Specialities &amp; Services</Heading>
-          <Divider mb="5" />
-          <SpecialtiesSection />
-        </Box>
-      </SpacedContainer>
+const SpecialtiesServices = () => (
+  <SpacedContainer py={10} mx={12}>
+    <Box
+      borderRadius="3xl"
+      borderColor="brand.darkPink"
+      boxShadow="0 10px 30px 0 rgb(205 204 219 / 25%)"
+      filter="drop-shadow(15px 70px 90px rgba(164, 43, 173, 0.08))"
+    >
+      <Heading as="h2" fontSize="xl" fontWeight={500} color="primary.dark" mb="5">Specialities &amp; Services</Heading>
+      <Divider mb="5" />
+      <SpecialtiesSection />
     </Box>
-  );
-};
+  </SpacedContainer>
+);
 
 export default SpecialtiesServices;
